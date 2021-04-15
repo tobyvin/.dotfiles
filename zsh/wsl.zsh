9c821f2ac834c08e8f0b0faa504d87db4546f478
Toby Vincent <tobyv13@gmail.com>
2021-04-15 18:04:58 -0500

    feat: moving to new gpg script

--- zsh/wsl.zsh
+++ zsh/wsl.zsh
@@ -2,6 +2,8 @@ path=( $path $HOME/.local/bin $HOME/.dotnet/tools )
 hash -d w=/mnt/c/Users/$USER
 alias cb=clip.exe
 alias wsl=wsl.exe
+alias ykman="/mnt/c/Program\ Files/Yubico/YubiKey\ Manager/ykman.exe"
+alias gpg-agent-relay=$ZSH_BASE/ssh/gpg-agent-relay.sh
 
 function wt() { powershell.exe -NoProfile -c "wt $@" }
 
@@ -46,10 +48,13 @@ function r-cut() {
   (( ${+aliases[cb]} )) && printf "$CUTBUFFER" | cb
 }
 
-# YubiKey - GPG: https://github.com/benpye/wsl-ssh-pageant
-export GPG_AGENT_SOCK=$HOME/.gnupg/S.gpg-agent
-ss -a | grep -q $GPG_AGENT_SOCK
-if [ $? -ne 0 ]; then
-  rm -rf $GPG_AGENT_SOCK
-  (setsid nohup socat UNIX-LISTEN:$GPG_AGENT_SOCK,fork EXEC:"$HOME/.ssh/wsl2-ssh-pageant.exe --gpg S.gpg-agent" >/dev/null 2>&1 &)
-fi
+# YubiKey - GPG: https://blog.nimamoh.net/yubi-key-gpg-wsl2/
+gpg-agent-relay start
+export SSH_AUTH_SOCK=$HOME/.gnupg/S.gpg-agent.ssh
+
+# export SSH_AUTH_SOCK=/tmp/wincrypt-hv.sock
+# ss -lnx | grep -q $SSH_AUTH_SOCK
+# if [ $? -ne 0 ]; then
+# 	rm -f $SSH_AUTH_SOCK
+#   (setsid nohup socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork SOCKET-CONNECT:40:0:x0000x33332222x02000000x00000000 >/dev/null 2>&1)
+# fi
