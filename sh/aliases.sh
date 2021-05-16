683d92085c0cb982fdedff6f9d5b93ee86899f51
Toby Vincent <tobyv13@gmail.com>
2021-05-16 18:20:59 -0500

    feat: added default omz aliases

--- sh/aliases.sh
+++ sh/aliases.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
 # Aliases
-alias update="sudo apt update && apt list --upgradable"
-alias upgrade="sudo apt upgrade -y"
+alias ls='ls --color=tty'
+alias l='ls -lah'
+alias la='ls -lAh'
+alias ll='ls -lh'
+alias lla='ls -lA'
+alias lsa='ls -lah'
+alias grep='grep --color'
 alias ipa="ip -s -c -h a"
 alias untar="tar -zxvf "
 alias py=python3
@@ -10,3 +15,17 @@ alias python=python3
 alias pip=pip3
 alias dsh="dce sh"
 alias dps="docker ps"
+alias update="sudo apt update && apt list --upgradable"
+alias upgrade="sudo apt upgrade -y"
+
+# Command line head / tail shortcuts
+alias -g H='| head'
+alias -g T='| tail'
+alias -g G='| grep'
+alias -g L="| less"
+alias -g M="| most"
+alias -g LL="2>&1 | less"
+alias -g CA="2>&1 | cat -A"
+alias -g NE="2> /dev/null"
+alias -g NUL="> /dev/null 2>&1"
+alias -g P="2>&1| pygmentize -l pytb"
