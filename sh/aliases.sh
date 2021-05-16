7865c9dab44c9912af3df9c29114e76fa91a4734
Toby Vincent <tobyv13@gmail.com>
2021-05-16 18:22:34 -0500

    feat: moved non posix aliases to aliases.zsh

--- sh/aliases.sh
+++ sh/aliases.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-# Aliases
 alias ls='ls --color=tty'
 alias l='ls -lah'
 alias la='ls -lAh'
@@ -17,15 +16,3 @@ alias dsh="dce sh"
 alias dps="docker ps"
 alias update="sudo apt update && apt list --upgradable"
 alias upgrade="sudo apt upgrade -y"
-
-# Command line head / tail shortcuts
-alias -g H='| head'
-alias -g T='| tail'
-alias -g G='| grep'
-alias -g L="| less"
-alias -g M="| most"
-alias -g LL="2>&1 | less"
-alias -g CA="2>&1 | cat -A"
-alias -g NE="2> /dev/null"
-alias -g NUL="> /dev/null 2>&1"
-alias -g P="2>&1| pygmentize -l pytb"
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,15 @@
 #!/usr/bin/env zsh
 
 source "${DOTFILES:="${HOME}/dotfiles"}/sh/aliases.sh"
+
+# Command line head / tail shortcuts
+alias -g H='| head'
+alias -g T='| tail'
+alias -g G='| grep'
+alias -g L="| less"
+alias -g M="| most"
+alias -g LL="2>&1 | less"
+alias -g CA="2>&1 | cat -A"
+alias -g NE="2> /dev/null"
+alias -g NUL="> /dev/null 2>&1"
+alias -g P="2>&1| pygmentize -l pytb"
