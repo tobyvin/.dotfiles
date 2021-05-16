41081eb98669b4e18ec9a8a0ea4be35cea3b6056
Toby Vincent <tobyv13@gmail.com>
2021-05-16 18:24:09 -0500

    fix: fixed formatting issue

--- sh/wsl.sh
+++ sh/wsl.sh
@@ -4,29 +4,30 @@ export EDITOR="code --wait"
 
 alias cb=clip.exe
 alias wsl=wsl.exe
-alias ykman="/mnt/c/Program\ Files/Yubico/YubiKey\ Manager/ykman.exe"
+alias ykman='/mnt/c/Program\ Files/Yubico/YubiKey\ Manager/ykman.exe'
 
-function wt() { powershell.exe -NoProfile -c "wt $@" }
+function wt() {
+  powershell.exe -NoProfile -c "wt $@"
+}
 
 function winget() {
   cmd="winget.exe $1"
   shift # past cmd
 
-  while [[ $# -gt 0 ]]
-  do
+  while [[ $# -gt 0 ]]; do
     key="$1"
     case $key in
-        -*) # key value pair
-        cmd+=" $key '$2'"
-        shift # past argument
-        shift # past value
-        ;;
-        *)    # positional argument
-        cmd+=" '$1'" # add it to the list
-        shift # past argument
-        ;;
+    -*) # key value pair
+      cmd+=" $key '$2'"
+      shift # past argument
+      shift # past value
+      ;;
+    *)             # positional argument
+      cmd+=" '$1'" # add it to the list
+      shift        # past argument
+      ;;
     esac
   done
-  
+
   powershell.exe -NoProfile -c "$cmd"
 }
