3133aac738ddfd2e9fcee5ff917c7e2830cac34f
Toby Vincent <tobyv13@gmail.com>
2021-07-13 12:01:50 -0500

    feat: added GPG_TTY export for vscode remote

--- sh/env.sh
+++ sh/env.sh
@@ -7,6 +7,7 @@ export FZF_CTRL_T_OPTS="--min-height 30 --preview-window down:60% --preview-wind
 
 export BAT_CONFIG_PATH="${DOTFILES}/bat.conf"
 export MANPAGER="sh -c 'col -bx | bat -l man -p'"
+export GPG_TTY=$(tty)
 
 # Path
 PATH=$PATH:$HOME/.local/bin:$HOME/.dotnet/tools:$HOME/.cargo/bin:$DOTFILES/scripts
