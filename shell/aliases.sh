40231ddda83e1352822e5c474c184df173b7c865
Toby Vincent <tobyv13@gmail.com>
2021-11-14 19:37:51 -0600

    feat: prompt colors and cleanup

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -7,16 +7,11 @@ alias ll='ls -lh'
 alias lla='ls -lA'
 alias lsa='ls -lah'
 alias grep='grep --color'
-alias cat='bat --paging=never'
 alias ipa="ip -s -c -h a"
 alias untar="tar -zxvf"
 # scripts
 alias dfi="$DOTFILES/install"
 alias dfu="$DOTFILES/update"
-alias initgit="init-git.sh"
-alias inittex="init-latex.sh"
-alias initsrc="init-src.sh"
-alias cht="cht.sh"
 # docker
 alias dexec="docker exec -it"
 alias dps="docker ps"
--- shell/env.sh
+++ shell/env.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 
-# https://github.com/junegunn/fzf#respecting-gitignore
+export GPG_TTY=$(tty)
+export BROWSER=wslview
+export STARSHIP_LOG="error"
+export BAT_CONFIG_PATH="${DOTFILES}/bat.conf"
+export MANPAGER="sh -c 'col -bx | bat -l man -p'"
 export FZF_BASE="${HOME}/.fzf"
 export FZF_PREVIEW_COMMAND="bat --style=numbers,changes --wrap never --color always {} || cat {} || tree -C {}"
 export FZF_CTRL_T_OPTS="--min-height 30 --preview-window down:60% --preview-window noborder --preview '($FZF_PREVIEW_COMMAND) 2> /dev/null'"
-
-export BAT_CONFIG_PATH="${DOTFILES}/bat.conf"
-export MANPAGER="sh -c 'col -bx | bat -l man -p'"
-export GPG_TTY=$(tty)
-export BROWSER=wslview
 
 # Path
 PATH=$PATH:$HOME/.local/bin:$HOME/.go/bin:$HOME/.dotnet/tools:$HOME/.cargo/bin:$DOTFILES/scripts:/usr/local/texlive/2021/bin/x86_64-linux
