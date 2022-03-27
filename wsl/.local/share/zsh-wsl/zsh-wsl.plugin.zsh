da06c20b672c99660e041937fd05c5f73aa06bb6
Toby Vincent <tobyv13@gmail.com>
2022-03-26 22:53:33 -0500

    feat: create zsh-wsl plugin

--- wsl/.local/share/zsh-wsl/zsh-wsl.plugin.zsh
+++ wsl/.local/share/zsh-wsl/zsh-wsl.plugin.zsh
@@ -1,5 +1,60 @@
+#!/usr/bin/env bash
+
+[ -n "${WSL_DISTRO_NAME+1}" ] || return 0
+
+WINHOME="/mnt/c/Users/$USER"
+[ "$PWD" = "$WINHOME" ] && cd
+
+mkdir -p /tmp/xdg
+
+export BROWSER=wslview
+export EDITOR="code --wait"
+export DISPLAY=$(grep -m 1 nameserver /etc/resolv.conf | awk '{print $2}'):0
+export XDG_RUNTIME_DIR=/tmp/xdg
 export SSH_AUTH_SOCK="$HOME/.ssh/agent.sock"
 export GPG_AGENT_SOCK="$HOME/.gnupg/S.gpg-agent"
+
+alias wsl=wsl.exe
+alias ykman='/mnt/c/Program\ Files/Yubico/YubiKey\ Manager/ykman.exe'
+alias pip2="DISPLAY= pip2"
+alias pip3="DISPLAY= pip3"
+alias pip3.7="DISPLAY= pip3.7"
+alias pip3.10="DISPLAY= pip3.10"
+alias pip="DISPLAY= pip3"
+
+wt() {
+  powershell.exe -NoProfile -c "wt $*"
+}
+
+wsl_cmd_proxy() {
+  exe="$1"
+  shift # past exe
+
+  cmd=" ${1}"
+  shift # past cmd
+
+  args=""
+  while [[ $# -gt 0 ]]; do
+    case "$1" in
+    -*)
+      args+="${1} '${2}' "
+      shift
+      shift
+      ;;
+    *)
+      args+="'${1}'"
+      shift
+      ;;
+    esac
+    args+=" "
+  done
+
+  powershell.exe -NoProfile -c 'cd $HOME;' "$exe" "$cmd" "$args"
+}
+
+winget() { wsl_cmd_proxy "winget.exe" "$@"; }
+scoop() { wsl_cmd_proxy "scoop" "$@"; }
+alacritty() { wsl_cmd_proxy "alacritty.exe" "$@"; }
 
 gpg-init() (
     if ! ss -a | grep -q "$SSH_AUTH_SOCK"; then
@@ -64,3 +119,6 @@ gpg-learn() {
 }
 
 gpg-init
+
+# https://github.com/validatedev/drop-cache-if-idle
+[ -z "$(ps -ef | grep cron | grep -v grep)" ] && sudo /etc/init.d/cron start &>/dev/null
