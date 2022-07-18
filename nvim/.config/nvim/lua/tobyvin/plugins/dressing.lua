4f1ea19457554299938554f6598f9d0e2eee91a5
Toby Vincent <tobyv13@gmail.com>
2022-07-18 14:22:51 -0500

    fix(nvim): fix issue with dressing format table lookup

--- nvim/.config/nvim/lua/tobyvin/plugins/dressing.lua
+++ nvim/.config/nvim/lua/tobyvin/plugins/dressing.lua
@@ -1,24 +1,13 @@
 local themes = require("telescope.themes")
+local backends = require("dressing.config").get_mod_config("select").backend
 local M = {}
 
-M.get_mt = function(kind)
-	return {
-		__index = function(v)
-			local backends = require("dressing.config").get_mod_config(kind).backend
-			if vim.tbl_contains(backends, v) then
-				return { backend = v }
-			end
-		end,
-	}
-end
-
 M.kinds = {
-	select = setmetatable({
+	select = {
 		select_normal = {
 			telescope = themes.get_dropdown({ initial_mode = "normal" }),
 		},
-	}, M.get_mt("input")),
-	input = setmetatable({}, M.get_mt("input")),
+	},
 }
 
 M.setup = function()
@@ -31,7 +20,11 @@ M.setup = function()
 	dressing.setup({
 		select = {
 			get_config = function(opts)
-				return M.kinds.select[opts.kind]
+				if vim.tbl_contains(M.kinds, opts.kind) then
+					return M.kinds.select[opts.kind]
+				elseif vim.tbl_contains(backends, opts.kind) then
+					return { backend = opts.kind }
+				end
 			end,
 		},
 	})
