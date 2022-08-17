65817c7572169bfa4695b3d90c1503701fc59fcf
Toby Vincent <tobyv13@gmail.com>
2022-08-16 21:48:20 -0500

    feat(nvim): improve telescope keybinds

--- nvim/.config/nvim/lua/tobyvin/plugins/telescope.lua
+++ nvim/.config/nvim/lua/tobyvin/plugins/telescope.lua
@@ -36,6 +36,9 @@ M.setup = function()
 				path = vim.fn.stdpath("data") .. "/databases/telescope_history.sqlite3",
 				limit = 100,
 			},
+			cache_picker = {
+				num_pickers = 10,
+			},
 		},
 		pickers = {
 			find_files = {
@@ -63,19 +66,30 @@ M.setup = function()
 	local builtins = require("telescope.builtin")
 
 	local nmap_find = require("tobyvin.utils").create_map_group("n", "<leader>f", { desc = "Find" })
+	nmap_find("a", builtins.autocommands, { desc = "Autocommands" })
 	nmap_find("b", builtins.buffers, { desc = "Buffers" })
 	nmap_find("c", builtins.command_history, { desc = "Command History" })
 	nmap_find("C", builtins.commands, { desc = "Commands" })
 	nmap_find("f", builtins.find_files, { desc = "Files" })
+	nmap_find("F", builtins.filetypes, { desc = "Filetypes" })
 	nmap_find("g", builtins.live_grep, { desc = "Grep" })
 	nmap_find("h", builtins.help_tags, { desc = "Help" })
+	nmap_find("H", builtins.highlights, { desc = "Highlights" })
+	nmap_find("j", builtins.jumplist, { desc = "Jumplist" })
 	nmap_find("k", builtins.keymaps, { desc = "Keymaps" })
-	nmap_find("r", builtins.resume, { desc = "Resume" })
-	nmap_find("m", builtins.man_pages, { desc = "Man Pages" })
+	nmap_find("l", builtins.loclist, { desc = "Loclist" })
+	nmap_find("m", builtins.marks, { desc = "Marks" })
+	nmap_find("M", builtins.man_pages, { desc = "Man Pages" })
 	nmap_find("o", builtins.oldfiles, { desc = "Old Files" })
-	nmap_find("'", builtins.registers, { desc = "Registers" })
+	nmap_find("p", builtins.pickers, { desc = "Pickers" })
+	nmap_find("r", builtins.resume, { desc = "Resume" })
+	nmap_find("R", builtins.reloader, { desc = "Reloader" })
+	nmap_find("s", builtins.spell_suggest, { desc = "Spell Suggest" })
+	nmap_find("S", builtins.search_history, { desc = "Search History" })
+	nmap_find("t", builtins.tags, { desc = "Tags" })
 	nmap_find("t", builtins.colorscheme, { desc = "Colorscheme" })
-	nmap_find("p", telescope.extensions.packer.packer, { desc = "Packer" })
+	nmap_find("v", builtins.vim_options, { desc = "Vim Options" })
+	nmap_find("'", builtins.registers, { desc = "Registers" })
 
 	local nmap_git = require("tobyvin.utils").create_map_group("n", "<leader>g", { desc = "Git" })
 	nmap_git("b", builtins.git_branches, { desc = "Checkout branch" })
@@ -91,14 +105,9 @@ M.setup = function()
 	nmap_git_gh("p", telescope.extensions.gh.pull_request, { desc = "Pull request" })
 	nmap_git_gh("g", telescope.extensions.gh.gist, { desc = "Gist" })
 	nmap_git_gh("r", telescope.extensions.gh.run, { desc = "Run" })
-end
 
-M.project_files = function()
-	local opts = {} -- define here if you want to define something
-	local ok = pcall(require("telescope.builtin").git_files, opts)
-	if not ok then
-		require("telescope.builtin").find_files(opts)
-	end
+	local nmap_packer = require("tobyvin.utils").create_map_group("n", "<leader>p", { desc = "Packer" })
+	nmap_packer("f", telescope.extensions.packer.packer, { desc = "Find plugins" })
 end
 
 return M
