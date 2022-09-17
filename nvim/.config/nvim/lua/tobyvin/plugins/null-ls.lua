6dbac00cbec2d8b6491d2fbfd4ba810468d8f9b6
Toby Vincent <tobyv13@gmail.com>
2022-09-17 09:25:48 -0500

    refactor(nvim): remove unused null-ls modules

--- nvim/.config/nvim/lua/tobyvin/plugins/null-ls.lua
+++ nvim/.config/nvim/lua/tobyvin/plugins/null-ls.lua
@@ -9,26 +9,20 @@ M.setup = function()
 	end
 
 	local code_actions = null_ls.builtins.code_actions
-	local diagnostics = null_ls.builtins.diagnostics
 	local formatting = null_ls.builtins.formatting
 
 	null_ls.setup({
 		sources = {
-
 			-- Code Actions
 			code_actions.gitrebase,
 			-- Disabled until sorting for code_actions gets pushed. See: https://github.com/stevearc/dressing.nvim/issues/22
 			-- code_actions.gitsigns,
 			code_actions.shellcheck,
 
-			-- Diagnostics
-			diagnostics.checkmake,
-
 			-- Formatting
 			formatting.prettier,
 			formatting.black,
 			formatting.latexindent,
-			formatting.markdownlint,
 			formatting.stylua,
 			formatting.shfmt,
 		},
