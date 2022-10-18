0e355edd40eed62e8ad7f79db88f4ae03680be05
Toby Vincent <tobyv13@gmail.com>
2022-10-18 09:35:14 -0500

    fix(nvim): fix typo in tokyonight setup

--- nvim/.config/nvim/lua/tobyvin/plugins/tokyonight.lua
+++ nvim/.config/nvim/lua/tobyvin/plugins/tokyonight.lua
@@ -1,13 +1,17 @@
 local M = {}
 
 M.setup = function()
-	local status_ok, tokyonight = pcall(require, "gruvbox")
+	local status_ok, tokyonight = pcall(require, "tokyonight")
 	if not status_ok then
 		vim.notify("Failed to load module 'tokyonight'", vim.log.levels.ERROR)
 		return
 	end
 
 	tokyonight.setup({
+		styles = {
+			sidebars = "transparent",
+			floats = "transparent",
+		},
 		transparent = true,
 	})
 end
