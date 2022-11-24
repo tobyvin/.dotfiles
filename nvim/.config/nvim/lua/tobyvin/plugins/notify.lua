1799de0790f9a463d9206566d37a3e389fd4d72e
Toby Vincent <tobyv13@gmail.com>
2022-11-24 16:14:07 -0600

    refactor(nvim): use NotifyBackground hl to set notify background

--- nvim/.config/nvim/lua/tobyvin/plugins/notify.lua
+++ nvim/.config/nvim/lua/tobyvin/plugins/notify.lua
@@ -7,16 +7,14 @@ M.setup = function()
 		return
 	end
 
-	notify.setup({
-		background_colour = "#" .. vim.api.nvim_get_hl_by_name("Pmenu", true).background,
-	})
-
 	vim.api.nvim_create_autocmd("User", {
 		pattern = "Notify",
 		callback = function(args)
 			notify.notify(unpack(args.data))
 		end,
 	})
+
+	vim.api.nvim_set_hl(0, "NotifyBackground", { link = "WinBar" })
 
 	local telescope_ok, telescope = pcall(require, "telescope")
 	if telescope_ok then
