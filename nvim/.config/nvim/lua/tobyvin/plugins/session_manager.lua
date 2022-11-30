3c95930188bfd89a16331d534c2173a2495130d7
Toby Vincent <tobyv13@gmail.com>
2022-11-30 13:59:53 -0600

    refactor(nvim): reword session_manager keymap desc

--- nvim/.config/nvim/lua/tobyvin/plugins/session_manager.lua
+++ nvim/.config/nvim/lua/tobyvin/plugins/session_manager.lua
@@ -9,7 +9,7 @@ session_manager.setup({
 })
 
 vim.keymap.set("n", "<leader>ss", session_manager.save_current_session, { desc = "save session" })
-vim.keymap.set("n", "<leader>sl", session_manager.load_current_dir_session, { desc = "load current session" })
-vim.keymap.set("n", "<leader>sL", session_manager.load_session, { desc = "load session" })
-vim.keymap.set("n", "<leader>sr", session_manager.load_last_session, { desc = "load last session" })
+vim.keymap.set("n", "<leader>sl", session_manager.load_current_dir_session, { desc = "load session" })
+vim.keymap.set("n", "<leader>sL", session_manager.load_session, { desc = "pick session" })
+vim.keymap.set("n", "<leader>sr", session_manager.load_last_session, { desc = "last session" })
 vim.keymap.set("n", "<leader>sd", session_manager.delete_session, { desc = "delete session" })
