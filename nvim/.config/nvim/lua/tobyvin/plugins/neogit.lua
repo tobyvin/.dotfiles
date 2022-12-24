d40d801053f2f4a50514739969578a788c6fe48b
Toby Vincent <tobyv13@gmail.com>
2022-12-24 15:48:50 -0600

    feat(nvim,neogit): manually impl pr until it is merged
    Refs: #415

--- nvim/.config/nvim/lua/tobyvin/plugins/neogit.lua
+++ nvim/.config/nvim/lua/tobyvin/plugins/neogit.lua
@@ -1,19 +1,76 @@
 local M = {
 	"TimUntersberger/neogit",
 	dependencies = { "sindrets/diffview.nvim" },
-	config = {
-		disable_commit_confirmation = true,
-		disable_signs = true,
-		integrations = {
-			diffview = true,
-		},
-	},
 }
 
 function M.init()
 	vim.keymap.set("n", "<leader>gg", function()
 		require("neogit").open()
 	end, { desc = "neogit" })
+end
+
+function M.config()
+	-- TODO: revert once pr is merged
+	--
+	-- Refs: #415
+	local Buffer = require("neogit.lib.buffer")
+	local config = require("neogit.config")
+	local input = require("neogit.lib.input")
+
+	local CommitEditor = require("neogit.buffers.commit_editor")
+	function CommitEditor:open()
+		local written = false
+		self.buffer = Buffer.create({
+			name = self.filename,
+			filetype = "gitcommit",
+			buftype = "",
+			kind = config.values.commit_popup.kind,
+			modifiable = true,
+			readonly = false,
+			autocmds = {
+				["BufWritePost"] = function()
+					written = true
+				end,
+				["BufUnload"] = function()
+					if written then
+						if
+							config.values.disable_commit_confirmation
+							or input.get_confirmation("Are you sure you want to commit?")
+						then
+							vim.cmd("silent g/^#/d | silent w!")
+						end
+					end
+					if self.on_unload then
+						self.on_unload(written)
+					end
+					require("neogit.process").defer_show_preview_buffers()
+				end,
+			},
+			mappings = {
+				n = {
+					["q"] = function(buffer)
+						buffer:close(true)
+					end,
+				},
+			},
+			initialize = function(buffer)
+				buffer:set_lines(0, -1, false, self.content)
+				if not config.values.disable_insert_on_commit then
+					vim.cmd(":startinsert")
+				end
+				-- NOTE: This avoids the user having to force to save the contents of the buffer.
+				vim.cmd("silent w!")
+			end,
+		})
+	end
+
+	require("neogit").setup({
+		disable_commit_confirmation = true,
+		disable_signs = true,
+		integrations = {
+			diffview = true,
+		},
+	})
 end
 
 return M
