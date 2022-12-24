bb90344c6d0378505a96a31ab11f9eb1fe9867c3
Toby Vincent <tobyv13@gmail.com>
2022-12-24 16:18:19 -0600

    feat(nvim): add todo-comments.nvim

---
+++ nvim/.config/nvim/lua/tobyvin/plugins/todo-comments.lua
@@ -0,0 +1,18 @@
+local M = {
+	"folke/todo-comments.nvim",
+	cmd = { "TodoTrouble", "TodoTelescope" },
+	event = "BufReadPost",
+	config = true,
+}
+
+function M.init()
+	vim.keymap.set("n", "]t", function()
+		require("todo-comments").jump_next()
+	end, { desc = "next todo" })
+
+	vim.keymap.set("n", "[t", function()
+		require("todo-comments").jump_prev()
+	end, { desc = "previous todo" })
+end
+
+return M
