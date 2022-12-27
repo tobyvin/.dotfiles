a21c7c9c2e291c572c4e372a878348ad04927cc9
Toby Vincent <tobyv13@gmail.com>
2022-12-27 14:27:50 -0600

    perf(nvim): add true lazy loading for dressing

--- nvim/.config/nvim/lua/tobyvin/plugins/dressing.lua
+++ nvim/.config/nvim/lua/tobyvin/plugins/dressing.lua
@@ -1,10 +1,23 @@
 local M = {
 	"stevearc/dressing.nvim",
-	event = "VeryLazy",
 	dependencies = {
 		"nvim-telescope/telescope.nvim",
 	},
 }
+
+function M.init()
+	---@diagnostic disable-next-line: duplicate-set-field
+	vim.ui.select = function(...)
+		require("lazy").load({ plugins = { "dressing.nvim" } })
+		return vim.ui.select(...)
+	end
+
+	---@diagnostic disable-next-line: duplicate-set-field
+	vim.ui.input = function(...)
+		require("lazy").load({ plugins = { "dressing.nvim" } })
+		return vim.ui.input(...)
+	end
+end
 
 function M.config()
 	local dressing = require("dressing")
