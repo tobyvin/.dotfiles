301b103da1ea4e765890d7eb8e4a1d84c3dc14f5
Toby Vincent <tobyv13@gmail.com>
2022-12-28 13:14:57 -0600

    feat(nvim): add keymaps for refactoring.nvim

--- nvim/.config/nvim/lua/tobyvin/plugins/refactoring.lua
+++ nvim/.config/nvim/lua/tobyvin/plugins/refactoring.lua
@@ -1,5 +1,8 @@
 local M = {
 	"ThePrimeagen/refactoring.nvim",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+	},
 	config = true,
 }
 
@@ -7,6 +10,18 @@ function M.init()
 	vim.keymap.set("v", "<leader>rr", function()
 		require("refactoring").select_refactor()
 	end, { desc = "refactor" })
+
+	vim.keymap.set("n", "<leader>ri", function()
+		require("refactoring").refactor("Inline Variable")
+	end, { desc = "inline variable" })
+
+	vim.keymap.set("n", "<leader>rb", function()
+		require("refactoring").refactor("Extract Block")
+	end, { desc = "extract block" })
+
+	vim.keymap.set("n", "<leader>rf", function()
+		require("refactoring").refactor("Extract Block To File")
+	end, { desc = "extract block to file" })
 end
 
 return M
