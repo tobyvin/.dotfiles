1424615011b525bf7423ab03d14803d4fd83a42b
Toby Vincent <tobyv13@gmail.com>
2022-12-28 13:15:35 -0600

    feat(nvim): enable plugins for luadev in dotfiles

--- nvim/.config/nvim/lua/tobyvin/plugins/neodev.lua
+++ nvim/.config/nvim/lua/tobyvin/plugins/neodev.lua
@@ -10,7 +10,7 @@ function M.config()
 		override = function(root_dir, library)
 			local lua_dev_utils = require("lua-dev.util")
 			if lua_dev_utils.has_file(root_dir, lua_dev_utils.fqn("~/.dotfiles/nvim/.config/nvim")) then
-				library.enabled = true
+				library.plugins = true
 			end
 		end,
 	})
