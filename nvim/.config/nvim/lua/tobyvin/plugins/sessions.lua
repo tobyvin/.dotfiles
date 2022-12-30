89ac4ffc6d367a7d11af47549d463b21603f055c
Toby Vincent <tobyv13@gmail.com>
2022-12-29 20:08:44 -0600

    fix(nvim): fix session name and create missing session directory

--- nvim/.config/nvim/lua/tobyvin/plugins/sessions.lua
+++ nvim/.config/nvim/lua/tobyvin/plugins/sessions.lua
@@ -2,16 +2,27 @@ local M = {
 	"echasnovski/mini.sessions",
 	name = "sessions",
 	event = { "VimLeavePre" },
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
 	config = true,
 }
 
 function M.init()
+	local session_name = function()
+		if vim.v.this_session ~= "" then
+			return vim.v.this_session
+		end
+		local name, _ = vim.loop.cwd():gsub(":", "++"):gsub(require("plenary.path").path.sep, "%%")
+		return name
+	end
+
 	vim.keymap.set("n", "<leader>sw", function()
-		require("mini.sessions").write()
+		require("mini.sessions").write(session_name())
 	end, { desc = "write session" })
 
 	vim.keymap.set("n", "<leader>sr", function()
-		require("mini.sessions").read()
+		require("mini.sessions").read(session_name())
 	end, { desc = "read session" })
 
 	vim.keymap.set("n", "<leader>ss", function()
@@ -19,11 +30,13 @@ function M.init()
 	end, { desc = "select session" })
 
 	vim.keymap.set("n", "<leader>sd", function()
-		require("mini.sessions").delete()
+		require("mini.sessions").delete(session_name())
 	end, { desc = "delete session" })
 end
 
 function M.config()
+	require("plenary.path"):new(vim.fn.stdpath("data")):joinpath("session"):mkdir()
+
 	require("mini.sessions").setup()
 end
 
