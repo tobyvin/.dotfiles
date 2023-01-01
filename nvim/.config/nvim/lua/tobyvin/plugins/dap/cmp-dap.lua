229f851cd0b7e56bb7f84569a4036eea85e4cab5
Toby Vincent <tobyv13@gmail.com>
2023-01-01 17:19:02 -0600

    fix(nvim): remove dapui related options

--- nvim/.config/nvim/lua/tobyvin/plugins/dap/cmp-dap.lua
+++ nvim/.config/nvim/lua/tobyvin/plugins/dap/cmp-dap.lua
@@ -1,13 +1,13 @@
 local M = {
 	"rcarriga/cmp-dap",
-	ft = { "dap-repl", "dapui_watches", "dapui_hover" },
+	ft = { "dap-repl" },
 	dependencies = {
 		"hrsh7th/nvim-cmp",
 	},
 }
 
 function M.config()
-	require("cmp").setup.filetype({ "dap-repl", "dapui_watches", "dapui_hover" }, {
+	require("cmp").setup.filetype({ "dap-repl" }, {
 		sources = {
 			{ name = "dap" },
 		},
