a6e434890ef1f9bcca59f8f227259a14aaee5469
Toby Vincent <tobyv13@gmail.com>
2023-01-11 19:05:27 -0600

    chore(nvim,deps): update plugins

--- nvim/.config/nvim/lua/tobyvin/plugins/neodev.lua
+++ nvim/.config/nvim/lua/tobyvin/plugins/neodev.lua
@@ -2,9 +2,6 @@ local M = {
 	"folke/neodev.nvim",
 	version = "*",
 	opts = {
-		experimental = {
-			pathStrict = true,
-		},
 		override = function(root_dir, library)
 			if root_dir:match("dotfiles") then
 				library.enabled = true
