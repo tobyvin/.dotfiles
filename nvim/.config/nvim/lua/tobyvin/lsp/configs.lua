55e0d5ff389db39e63e010298d867503f1e7d343
Toby Vincent <tobyv13@gmail.com>
2023-01-18 16:48:33 -0600

    fix(nvim): improve texlab config

--- nvim/.config/nvim/lua/tobyvin/lsp/configs.lua
+++ nvim/.config/nvim/lua/tobyvin/lsp/configs.lua
@@ -8,133 +8,135 @@ local M = {
 	stylelint_lsp = {},
 	clangd = {},
 	pylsp = {},
-}
-
-M.jsonls = {
-	settings = {
-		json = {
-			format = {
-				enable = true,
+	perlnavigator = {
+		settings = {
+			perlnavigator = {
+				enableWarnings = false,
 			},
-			validate = { enable = true },
 		},
 	},
-}
-
-M.gopls = {
-	cmd = { "gopls", "serve" },
-	settings = {
-		gopls = {
-			analyses = {
-				unusedparams = true,
+	jsonls = {
+		settings = {
+			json = {
+				format = {
+					enable = true,
+				},
+				validate = { enable = true },
 			},
-			staticcheck = true,
 		},
 	},
-}
-
-M.rust_analyzer = {
-	standalone = true,
-	settings = {
-		["rust-analyzer"] = {
-			cargo = {
-				features = "all",
+	gopls = {
+		cmd = { "gopls", "serve" },
+		settings = {
+			gopls = {
+				analyses = {
+					unusedparams = true,
+				},
+				staticcheck = true,
 			},
-			check = {
-				command = "clippy",
+		},
+	},
+	rust_analyzer = {
+		standalone = true,
+		settings = {
+			["rust-analyzer"] = {
+				cargo = {
+					features = "all",
+				},
+				check = {
+					command = "clippy",
+				},
+				completion = {
+					postfix = {
+						enable = false,
+					},
+				},
+				imports = {
+					granularity = {
+						enforce = true,
+					},
+				},
 			},
-			completion = {
-				postfix = {
+		},
+	},
+	sumneko_lua = {
+		settings = {
+			Lua = {
+				workspace = {
+					checkThirdParty = false,
+				},
+				completion = {
+					callSnippet = "Replace",
+				},
+				diagnostics = {
+					globals = { "vim" },
+				},
+				format = {
 					enable = false,
 				},
 			},
-			imports = {
-				granularity = {
-					enforce = true,
+		},
+	},
+	texlab = {
+		settings = {
+			texlab = {
+				rootDirectory = ".",
+				build = {
+					args = {
+						"-pdf",
+						"-interaction=nonstopmode",
+						"-synctex=1",
+						"-auxdir=../build",
+						"-outdir=../build",
+						"-emulate-aux-dir",
+						"%f",
+					},
+					onSave = true,
+				},
+				forwardSearch = {
+					executable = "zathura",
+					args = {
+						"--synctex-editor-command",
+						[[nvim-texlabconfig -file '%{input}' -line %{line}]],
+						"--synctex-forward",
+						"%l:1:%f",
+						"%p",
+					},
+				},
+				chktex = {
+					onEdit = true,
+					onOpenAndSave = true,
+				},
+				auxDirectory = "../build",
+				latexindent = {
+					["local"] = string.format("%s/latexindent/indentconfig.yaml", vim.env.XDG_CONFIG_HOME),
+					modifyLineBreaks = true,
 				},
 			},
 		},
+		on_attach = function(_, bufnr)
+			vim.b[bufnr].tex_flavor = "latex"
+			vim.wo.spell = true
+
+			local preview_autocmd
+			local augroup = vim.api.nvim_create_augroup("texlab", {})
+
+			vim.api.nvim_create_user_command("TexlabPreview", function()
+				preview_autocmd = vim.api.nvim_create_autocmd("CursorMoved", {
+					group = augroup,
+					command = "TexlabForward",
+				})
+
+				vim.cmd.TexlabForward()
+			end, { desc = "Texlab preview start" })
+
+			vim.api.nvim_create_user_command("TexlabPreviewStop", function()
+				if preview_autocmd then
+					vim.api.nvim_del_autocmd(preview_autocmd)
+				end
+			end, { desc = "Texlab preview stop" })
+		end,
 	},
-}
-
-M.sumneko_lua = {
-	settings = {
-		Lua = {
-			workspace = {
-				checkThirdParty = false,
-			},
-			completion = {
-				callSnippet = "Replace",
-			},
-			diagnostics = {
-				globals = { "vim" },
-			},
-			format = {
-				enable = false,
-			},
-		},
-	},
-}
-
-M.texlab = {
-	settings = {
-		texlab = {
-			rootDirectory = ".",
-			build = {
-				args = {
-					"-pdf",
-					"-interaction=nonstopmode",
-					"-synctex=1",
-					"-auxdir=../build",
-					"-outdir=../build",
-					"-emulate-aux-dir",
-					"%f",
-				},
-				onSave = true,
-			},
-			forwardSearch = {
-				executable = "zathura",
-				args = {
-					"--synctex-editor-command",
-					[[nvim-texlabconfig -file '%{input}' -line %{line}]],
-					"--synctex-forward",
-					"%l:1:%f",
-					"%p",
-				},
-			},
-			chktex = {
-				onEdit = true,
-				onOpenAndSave = true,
-			},
-			auxDirectory = "../build",
-			latexindent = {
-				["local"] = string.format("%s/latexindent/indentconfig.yaml", vim.env.XDG_CONFIG_HOME),
-				modifyLineBreaks = true,
-			},
-		},
-	},
-	on_attach = function(_, bufnr)
-		vim.b[bufnr].tex_flavor = "latex"
-		vim.wo.spell = true
-
-		local preview_autocmd
-		local augroup = vim.api.nvim_create_augroup("texlab", {})
-
-		vim.api.nvim_create_user_command("TexlabPreview", function()
-			preview_autocmd = vim.api.nvim_create_autocmd("CursorMoved", {
-				group = augroup,
-				command = "TexlabForward",
-			})
-
-			vim.cmd.TexlabForward()
-		end, { desc = "Texlab preview start" })
-
-		vim.api.nvim_create_user_command("TexlabPreviewStop", function()
-			if preview_autocmd then
-				vim.api.nvim_del_autocmd(preview_autocmd)
-			end
-		end, { desc = "Texlab preview stop" })
-	end,
 }
 
 return M
