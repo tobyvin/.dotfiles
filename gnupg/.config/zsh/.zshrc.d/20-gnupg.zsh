850a5722b7b082600d9d2efed1d3667656910476
Toby Vincent <tobyv13@gmail.com>
2023-03-01 18:04:18 -0600

    fix(gnupg): forward stderr when updating tty

--- gnupg/.config/zsh/.zshrc.d/20-gnupg.zsh
+++ gnupg/.config/zsh/.zshrc.d/20-gnupg.zsh
@@ -4,6 +4,6 @@
 GPG_TTY=$(tty)
 export GPG_TTY
 
-gpg-connect-agent updatestartuptty /bye >/dev/null
+gpg-connect-agent updatestartuptty /bye >/dev/null 2>&1
 
 alias unlock='echo "" | gpg --clearsign 1>/dev/null && ssh localhost -- : 1>/dev/null'
