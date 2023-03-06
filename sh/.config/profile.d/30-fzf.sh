8ea8a5f2d08a258c7eeced382aa921e4a1190896
Toby Vincent <tobyv13@gmail.com>
2023-03-06 15:39:45 -0600

    fix(fzf): improve fzf-tmux args

--- sh/.config/profile.d/30-fzf.sh
+++ sh/.config/profile.d/30-fzf.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-export FZF_TMUX_OPTS="-p"
+export FZF_TMUX="${TMUX:+1}"
+export FZF_TMUX_OPTS='-p80%,80%'
 export FZF_PREVIEW_COMMAND='less {} 2>/dev/null'
 export FZF_DEFAULT_COMMAND="fd --type f || git ls-tree -r --name-only HEAD || rg --files || find ."
 export FZF_DEFAULT_OPTS='--bind ctrl-q:abort
