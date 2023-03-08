c7198aaab1152aaae0e85a8dd2ac0263416bf982
Toby Vincent <tobyv13@gmail.com>
2023-03-08 16:36:45 -0600

    fix(gnupg): seperate gpg and ssh configs

--- gnupg/.config/profile.d/20-gnupg.sh
+++ gnupg/.config/profile.d/20-gnupg.sh
@@ -2,9 +2,3 @@
 
 GPG_AGENT_SOCK="$(gpgconf --list-dirs agent-socket)"
 export GPG_AGENT_SOCK
-
-unset SSH_AGENT_PID
-if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
-	SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
-	export SSH_AUTH_SOCK
-fi
---
+++ gnupg/.config/profile.d/20-ssh.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+unset SSH_AGENT_PID
+if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
+	SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
+	export SSH_AUTH_SOCK
+fi
