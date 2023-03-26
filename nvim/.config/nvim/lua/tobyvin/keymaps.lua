cf434f42464d583bc5285fd6ad49d80b3112a370
Toby Vincent <tobyv13@gmail.com>
2023-03-25 19:14:39 -0500

    feat(nvim,lsp): properly setup formatexpr

--- nvim/.config/nvim/lua/tobyvin/keymaps.lua
+++ nvim/.config/nvim/lua/tobyvin/keymaps.lua
@@ -1,6 +1,13 @@
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "up half page and center" })
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "down half page and center" })
+
 vim.keymap.set("n", "<a-j>", "<CMD>m +1<CR>", { desc = "move line down" })
 vim.keymap.set("n", "<a-k>", "<CMD>m -2<CR>", { desc = "move line up" })
 vim.keymap.set("v", "<a-k>", "<CMD>m '<-2<CR>gv=gv", { desc = "move selection up" })
 vim.keymap.set("v", "<a-j>", "<CMD>m '>+1<CR>gv=gv", { desc = "move selection down" })
+
+vim.keymap.set("n", "gqq", function()
+	local cursor = vim.api.nvim_win_get_cursor(0)
+	vim.cmd.normal("gggqG")
+	vim.api.nvim_win_set_cursor(0, cursor)
+end, { desc = "format buffer" })
--- nvim/.config/nvim/lua/tobyvin/lsp.lua
+++ nvim/.config/nvim/lua/tobyvin/lsp.lua
@@ -30,15 +30,20 @@ vim.api.nvim_create_autocmd("LspAttach", {
 			vim.keymap.set("n", "gx", vim.lsp.buf.external_docs, { desc = "external_docs", buffer = args.buf })
 		end
 
+		if client.server_capabilities.documentFormattingProvider then
+			vim.keymap.set("n", "gqq", vim.lsp.buf.format, { desc = "format", buffer = args.buf })
+		end
+
+		vim.keymap.set({ "n", "v" }, "<leader>lf", vim.lsp.buf.format, { desc = "format", buffer = args.buf })
+
 		vim.keymap.set({ "n", "v" }, "K", vim.lsp.buf.hover, { desc = "hover", buffer = args.buf })
-		vim.keymap.set({ "n", "v" }, "<C-k>", vim.lsp.buf.signature_help, { desc = "signature help", buffer = args.buf })
+		vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, { desc = "signature help", buffer = args.buf })
 		vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "definition", buffer = args.buf })
 		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "declaration", buffer = args.buf })
 		vim.keymap.set("n", "go", vim.lsp.buf.type_definition, { desc = "type definition", buffer = args.buf })
 		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { desc = "implementation", buffer = args.buf })
 		vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "references", buffer = args.buf })
 
-		vim.keymap.set({ "n", "v" }, "<leader>lf", vim.lsp.buf.format, { desc = "format", buffer = args.buf })
 		vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, { desc = "rename", buffer = args.buf })
 		vim.keymap.set("n", "<leader>la", vim.lsp.buf.code_action, { desc = "code action", buffer = args.buf })
 		vim.keymap.set("n", "<leader>ll", vim.lsp.codelens.run, { desc = "codelens", buffer = args.buf })
