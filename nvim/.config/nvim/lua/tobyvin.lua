522aa689dff1e8af14865a1f7f951740d43c8794
Toby Vincent <tobyv13@gmail.com>
2023-05-01 16:19:04 -0500

    fix(nvim): set eml filetype to mail and set textwidth to 72

--- nvim/.config/nvim/lua/tobyvin.lua
+++ nvim/.config/nvim/lua/tobyvin.lua
@@ -1,4 +1,6 @@
 require("tobyvin.options")
+require("tobyvin.filetype")
+require("tobyvin.autocmds")
 require("tobyvin.keymaps")
 require("tobyvin.lsp")
 require("tobyvin.lazy")
@@ -7,7 +9,6 @@ require("tobyvin.utils.dashboard")
 vim.api.nvim_create_autocmd("User", {
 	pattern = "VeryLazy",
 	callback = function()
-		require("tobyvin.autocmds")
 		require("tobyvin.diagnostic")
 		require("tobyvin.utils.session").setup()
 	end,
