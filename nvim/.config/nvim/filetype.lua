3e2fb4c7b12ef8e49783eb3e843a610a7567df4c
Toby Vincent <tobyv13@gmail.com>
2023-05-09 14:06:47 -0500

    refactor(nvim): unify filetype definitions

--- nvim/.config/nvim/filetype.lua
+++
@@ -1,6 +0,0 @@
-vim.filetype.add({
-	extension = {
-		ron = "ron",
-		zsh = "sh",
-	},
-})
--- nvim/.config/nvim/lua/tobyvin/filetype.lua
+++ nvim/.config/nvim/lua/tobyvin/filetype.lua
@@ -1,5 +1,10 @@
 vim.filetype.add({
 	extension = {
 		eml = "mail",
+		ron = "ron",
+		zsh = "sh",
+	},
+	pattern = {
+		[".*%.conf"] = { "dosini", { priority = -math.huge } },
 	},
 })
