91a109caf1234a30889e8bb6c895d85c8f268a67
Toby Vincent <tobyv13@gmail.com>
2023-05-14 10:54:25 -0500

    fix(nvim): fix sign definitions

--- nvim/.config/nvim/lua/tobyvin/diagnostic.lua
+++ nvim/.config/nvim/lua/tobyvin/diagnostic.lua
@@ -13,10 +13,10 @@ vim.diagnostic.config({
 	},
 })
 
-vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignHint" })
-vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignInfo" })
-vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignWarn" })
-vim.fn.sign_define("DiagnosticSignHint", { text = " ", texthl = "DiagnosticSignError" })
+vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
+vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
+vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
+vim.fn.sign_define("DiagnosticSignHint", { text = " ", texthl = "DiagnosticSignHint" })
 
 vim.keymap.set("n", "gl", vim.diagnostic.open_float, { desc = "open diagnostic float" })
 vim.keymap.set("n", "gL", vim.diagnostic.setqflist, { desc = "qf diagnostic" })
