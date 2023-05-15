fceeac4dbbd4b176bc28fdea23612e59dab51d5a
Toby Vincent <tobyv13@gmail.com>
2023-05-15 18:17:56 -0500

    fix(nvim): fix invalid plugin structure

--- nvim/.config/nvim/lua/plugins/dap.lua
+++ nvim/.config/nvim/lua/plugins/dap.lua
@@ -1,8 +1,50 @@
 local M = {
 	"mfussenegger/nvim-dap",
 	dependencies = {
-		require("plugins/dap/telescope-dap"),
-		require("plugins/dap/cmp-dap"),
+		{
+			"nvim-telescope/telescope-dap.nvim",
+			dependencies = {
+				"nvim-telescope/telescope.nvim",
+			},
+			init = function()
+				vim.keymap.set("n", "<leader>dC", function()
+					require("telescope").extensions.dap.commands()
+				end, { desc = "commands" })
+
+				vim.keymap.set("n", "<leader>dd", function()
+					require("telescope").extensions.dap.configurations()
+				end, { desc = "configurations" })
+
+				vim.keymap.set("n", "<leader>dl", function()
+					require("telescope").extensions.dap.list_breakpoints()
+				end, { desc = "breakpoints" })
+
+				vim.keymap.set("n", "<leader>df", function()
+					require("telescope").extensions.dap.frames()
+				end, { desc = "frames" })
+
+				vim.keymap.set("n", "<leader>dv", function()
+					require("telescope").extensions.dap.variables()
+				end, { desc = "variables" })
+			end,
+			config = function()
+				require("telescope").load_extension("dap")
+			end,
+		},
+		{
+			"rcarriga/cmp-dap",
+			ft = { "dap-repl" },
+			dependencies = {
+				"hrsh7th/nvim-cmp",
+			},
+			config = function()
+				require("cmp").setup.filetype({ "dap-repl" }, {
+					sources = {
+						{ name = "dap" },
+					},
+				})
+			end,
+		},
 		"mfussenegger/nvim-dap-python",
 		"leoluz/nvim-dap-go",
 		{
