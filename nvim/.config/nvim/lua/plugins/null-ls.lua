59d63e4c56ca81cee2b98f8f82c498ef53110a2a
Toby Vincent <tobyv13@gmail.com>
2023-05-16 12:18:46 -0500

    fix(nvim): remove markdownlint dup heading lint

--- nvim/.config/nvim/lua/plugins/null-ls.lua
+++ nvim/.config/nvim/lua/plugins/null-ls.lua
@@ -17,7 +17,11 @@ function M.config()
 			-- null_ls.builtins.code_actions.gitsigns,
 			null_ls.builtins.code_actions.gitrebase,
 			null_ls.builtins.code_actions.shellcheck,
-			null_ls.builtins.diagnostics.markdownlint,
+			null_ls.builtins.diagnostics.markdownlint.with({
+				extra_args = {
+					("--config=%s/markdownlint/markdownlint.yaml"):format(vim.env.XDG_CONFIG_HOME),
+				},
+			}),
 			null_ls.builtins.diagnostics.djlint,
 			null_ls.builtins.formatting.prettier.with({
 				disabled_filetypes = { "json", "jsonc" },
