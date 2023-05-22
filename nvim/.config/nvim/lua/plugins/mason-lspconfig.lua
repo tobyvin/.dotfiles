5848e8e82a848a24612377171df6f1d0639544ba
Toby Vincent <tobyv13@gmail.com>
2023-05-22 11:27:44 -0500

    fix(nvim): remove mason-lspconfig install autocmd

--- nvim/.config/nvim/lua/plugins/mason-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/mason-lspconfig.lua
@@ -22,30 +22,4 @@ local M = {
 	},
 }
 
-function M:init()
-	vim.api.nvim_create_autocmd("FileType", {
-		group = vim.api.nvim_create_augroup("mason-lspconfig", { clear = true }),
-		callback = function(args)
-			if vim.bo[args.buf].buftype ~= "" then
-				return
-			end
-
-			local available = require("mason-lspconfig").get_available_servers({ filetype = args.match })
-			if #available == 0 then
-				return
-			end
-
-			local installed = require("mason-lspconfig").get_installed_servers()
-			for _, server in ipairs(available) do
-				if vim.tbl_contains(installed, server) then
-					return
-				end
-			end
-
-			vim.schedule(vim.cmd.LspInstall)
-		end,
-		desc = "prompt to install missing lspconfig servers",
-	})
-end
-
 return M
