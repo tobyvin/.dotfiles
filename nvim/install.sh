8787612a3c73a7212f6b528aeff0b9262108a446
Toby Vincent <tobyv13@gmail.com>
2023-05-28 11:06:35 -0500

    build(nvim): clean plugins on install

--- nvim/install.sh
+++ nvim/install.sh
@@ -9,6 +9,7 @@ say() {
 # Update plugins
 say "Updating plugins to lock-file"
 nvim --headless -c 'Lazy! restore' -c qa
+nvim --headless -c 'Lazy! clean' -c qa
 
 # Update LSP servers
 say "Updating LSP servers"
