b5e26ed70d7379de5c36374ac5206ee243106344
Toby Vincent <tobyv13@gmail.com>
2023-06-23 16:46:34 -0500

    revert(01af646): breaks normal formatting
    This reverts commit 01af646 (fix(nvim): set nginx formatter, 2023-06-23).

--- nvim/.config/nvim/lua/plugins/null-ls.lua
+++ nvim/.config/nvim/lua/plugins/null-ls.lua
@@ -15,28 +15,6 @@ local M = {
 function M.config()
 	local null_ls = require("null-ls")
 
-	-- HACK: Workaround for null-ls unconditionally setting formatting capability
-	-- See: https://github.com/jose-elias-alvarez/null-ls.nvim/issues/1131
-	vim.api.nvim_create_autocmd("LspAttach", {
-		group = vim.api.nvim_create_augroup("null-ls", { clear = true }),
-		desc = "set formatexpr based on null-ls source",
-		callback = function(args)
-			local client = vim.lsp.get_client_by_id(args.data.client_id)
-
-			if
-				client.name == "null-ls"
-				and not require("null-ls.generators").can_run(
-					vim.bo[args.buf].filetype,
-					require("null-ls.methods").lsp.FORMATTING
-				)
-			then
-				pcall(vim.keymap.del, "n", "gqq", { buffer = args.buf })
-				pcall(vim.keymap.del, "n", "<leader>lf", { buffer = args.buf })
-				vim.bo[args.buf].formatexpr = nil
-			end
-		end,
-	})
-
 	null_ls.setup({
 		sources = {
 			-- Disabled until sorting for code_actions gets pushed. See: https://github.com/stevearc/dressing.nvim/issues/22
@@ -65,6 +43,7 @@ function M.config()
 			null_ls.builtins.formatting.shfmt.with({
 				extra_filetypes = { "PKGBUILD" },
 			}),
+			null_ls.builtins.formatting.nginx_beautifier,
 		},
 	})
 end
