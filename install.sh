99043d2e2183048a16ac4be4beddefc432332576
Toby Vincent <tobyv@tobyvin.dev>
2023-08-31 14:47:34 -0500

    build: fix symlink removal

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-# shellcheck disable=SC2035
+# shellcheck disable=SC2035,SC2016
 
 CDPATH='' cd -- "$(dirname -- "$0")" || exit
 
 printf "%s: Removing bad links\n" "$0"
-chkstow -t .. -b | cut -c13- | xargs -r rm -v
+fd . .. -Htl -E ./**/ -x sh -c '[ -e "{}" ] || (readlink -m "{}" | grep '"$PWD"' -q && rm -v "{}")'
 
 printf "%s: Stowing packages\n" "$0"
 stow "$@" */
