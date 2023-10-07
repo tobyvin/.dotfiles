5bc694f838a0dffca005972c9ea4e31cce28852a
Toby Vincent <tobyv@tobyvin.dev>
2023-10-06 20:34:23 -0500

    fix(nvim): improve lsp external docs handler

--- nvim/.config/nvim/lua/tobyvin/lsp/handlers.lua
+++ nvim/.config/nvim/lua/tobyvin/lsp/handlers.lua
@@ -1,14 +1,24 @@
-function vim.lsp.buf.external_docs()
-	local params = vim.lsp.util.make_position_params()
-	return vim.lsp.buf_request(0, "experimental/externalDocs", params)
-end
-
-return {
+local M = {
 	["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
 		border = "single",
 	}),
-
 	["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
 		border = "single",
 	}),
+	["experimental/externalDocs"] = function(_, url)
+		if url then
+			if vim.fn.executable("xdg-open") == 1 then
+				require("plenary.job"):new({ command = "xdg-open", args = { url } }):start()
+			else
+				pcall(vim.fn["netrw#BrowseX"], url, 0)
+			end
+		end
+	end,
 }
+
+function vim.lsp.buf.external_docs()
+	local params = vim.lsp.util.make_position_params()
+	return vim.lsp.buf_request(0, "experimental/externalDocs", params, M["experimental/externalDocs"])
+end
+
+return M
