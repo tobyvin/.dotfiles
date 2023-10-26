79592b1c2905da30ed04366aff01067e676bb346
Toby Vincent <tobyv@tobyvin.dev>
2023-10-26 17:36:11 -0500

    fix(nvim): add neotest adapters and clean up config

--- nvim/.config/nvim/lua/plugins/neotest.lua
+++ nvim/.config/nvim/lua/plugins/neotest.lua
@@ -1,28 +1,33 @@
-local utils = require("tobyvin.utils")
-
 ---@type LazyPluginSpec
 local M = {
 	"nvim-neotest/neotest",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
+		"nvim-neotest/neotest-go",
+		"nvim-neotest/neotest-plenary",
+		"nvim-neotest/neotest-python",
 		"rouge8/neotest-rust",
 	},
 }
 
-function M:init()
-	utils.on_attach(function()
-		vim.keymap.set("n", "<leader>tt", function()
-			require("neotest").run.run()
-		end, { desc = "run nearest test" })
-	end, { name = "rust_analyzer" })
+function M:opts(opts)
+	return vim.tbl_extend("keep", opts, {
+		adapters = {
+			require("neotest-go"),
+			require("neotest-plenary"),
+			require("neotest-python")({ dap = { justMyCode = false } }),
+			require("neotest-rust"),
+		},
+	})
 end
 
-function M:config(opts)
-	opts.adapters = {
-		require("neotest-rust"),
-	}
-
-	require("neotest").setup(opts)
+function M:init()
+	vim.keymap.set("n", "<leader>tt", function()
+		require("neotest").run.run({ suite = false, strategy = "integrated" })
+	end, { desc = "run nearest test" })
+	vim.keymap.set("n", "<leader>td", function()
+		require("neotest").run.run({ suite = false, strategy = "dap" })
+	end, { desc = "run nearest test" })
 end
 
 return M
