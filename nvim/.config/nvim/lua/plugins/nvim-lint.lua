6540139b5fdfe36bc37c8cca19a220eda1353018
Toby Vincent <tobyv@tobyvin.dev>
2023-11-02 18:55:41 -0500

    feat(nvim): add selene

--- nvim/.config/nvim/lua/plugins/nvim-lint.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lint.lua
@@ -6,19 +6,14 @@ local M = {
 			django = { "djlint" },
 			htmldjango = { "djlint" },
 			["jinja.html"] = { "djlint" },
+			lua = { "selene" },
 			markdown = { "markdownlint" },
 		},
 	},
 }
 
 function M:init()
-	vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-		callback = function()
-			require("lint").try_lint()
-		end,
-	})
-
-	vim.api.nvim_create_autocmd({ "BufReadPost" }, {
+	vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost", "InsertLeave" }, {
 		callback = function()
 			require("lint").try_lint()
 		end,
@@ -30,6 +25,19 @@ function M:config(opts)
 	markdownlint.args = {
 		"--config",
 		("%s/markdownlint/markdownlint.yaml"):format(vim.env.XDG_CONFIG_HOME),
+	}
+
+	local selene = require("lint").linters.selene
+	selene.args = {
+		"--display-style",
+		"json",
+		function()
+			local config = vim.fs.find({ "selene.toml" }, { path = vim.api.nvim_buf_get_name(0), upward = true })
+			if #config > 0 then
+				return string.format("--config=%s", config[1])
+			end
+		end,
+		"-",
 	}
 
 	require("lint").linters_by_ft = opts.linters_by_ft
