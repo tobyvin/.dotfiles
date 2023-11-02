8affd985aed58617f4367991ac603ab6350d4d3a
Toby Vincent <tobyv@tobyvin.dev>
2023-11-01 20:40:36 -0500

    fix: fix LESS envvar and remove from bat config

--- sh/.config/profile.d/20-less.sh
+++ sh/.config/profile.d/20-less.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-export LESS="--RAW-CONTROL-CHARS --quit-if-one-screen --mouse"
+export LESS="-RF --mouse"
 export LESSOPEN="|lesspipe.sh %s"
 export LESSHISTFILE="$XDG_STATE_HOME/lesshst"
