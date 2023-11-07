2815f126db9fcb84cd109cdc0f19b78998bf9d53
Toby Vincent <tobyv@tobyvin.dev>
2023-11-07 12:25:57 -0600

    fix(pg): add XDG Base Directory compat for pgsql

---
+++ sh/.config/profile.d/50-pg.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+export PSQLRC="$XDG_CONFIG_HOME/pg/psqlrc"
+export PSQL_HISTORY="$XDG_STATE_HOME/psql_history"
+export PGPASSFILE="$XDG_CONFIG_HOME/pg/pgpass"
+export PGSERVICEFILE="$XDG_CONFIG_HOME/pg/pg_service.conf"
