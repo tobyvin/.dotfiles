60b406e39f678ecf022ddac4775fa9e02066fb15
Toby Vincent <tobyv@tobyvin.dev>
2023-11-27 12:04:06 -0600

    fix(nvim): ferris lazy loading

--- nvim/.config/nvim/lua/plugins/ferris.lua
+++ nvim/.config/nvim/lua/plugins/ferris.lua
@@ -1,15 +1,16 @@
 ---@type LazyPluginSpec
 local M = {
 	"vxpm/ferris.nvim",
+  ft = { "rust" },
 	opts = {
 		url_handler = vim.ui.open,
 	},
 }
 
-function M:init()
-	U.on_attach(function()
-		require("ferris")
-	end, { name = "rust_analyzer" })
-end
+-- function M:init()
+-- 	U.on_attach(function()
+-- 		require("ferris")
+-- 	end, { name = "rust_analyzer" })
+-- end
 
 return M
