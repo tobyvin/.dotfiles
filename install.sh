e6aee455e9b9432f9646858d9853b7dae866f9b5
Toby Vincent <tobyv@tobyvin.dev>
2024-01-26 15:25:29 -0600

    build: track stowed untracked files
    This allows for the removal of symlinks of untracked files in the event
    that they are removed without ever being tracked by git.

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# shellcheck disable=SC2035,SC2016
+# shellcheck disable=SC2035
 
 set -e
 
@@ -7,17 +7,26 @@ CDPATH='' cd -- "$(dirname -- "$0")" || exit
 
 printf "%s: Removing bad links\n" "$0"
 {
-	git log --pretty=format: --name-only --no-renames --diff-filter=D
-	git status --porcelain --no-renames | grep -oP '^\s*\w*[DR?]+\w*\s*\K(.*)'
+	git log --name-only --no-renames --diff-filter=D --format=format:
+	git diff --name-only --no-renames --diff-filter=D HEAD
+	if [ -f .untracked ]; then
+		cat .untracked
+	fi
 } | sort -u | sed -n 's/^[^/]\+\//..\//p' | while read -r f; do
 	if [ -L "$f" ] && [ ! -e "$f" ]; then
 		rm -v "$f"
 	fi
 done
 
+git ls-files --exclude-standard --others >.untracked
+if [ ! -s .untracked ]; then
+	rm -f .untracked
+fi
+
 printf "%s: Stowing packages\n" "$0"
 stow "$@" */
 
+printf "%s: Installing packages\n" "$0"
 for f in */install.sh; do
 	$f
 done
