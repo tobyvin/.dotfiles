844100e9706eec1e21fea9d0d5a18fc47edca7c8
Toby Vincent <tobyv@tobyvin.dev>
2024-02-01 18:52:03 -0600

    fix(sheldon): remove unused plugins

--- fzf/.config/zsh/.zshrc.d/50-fzf.zsh
+++ fzf/.config/zsh/.zshrc.d/50-fzf.zsh
@@ -8,3 +8,7 @@ _fzf_compgen_path() {
 _fzf_compgen_dir() {
 	fd --type d --hidden --follow --exclude ".git" . "$1"
 }
+
+if [ -r "/usr/share/fzf/completion.zsh" ]; then
+	source /usr/share/fzf/completion.zsh
+fi
