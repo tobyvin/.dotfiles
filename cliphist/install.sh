57760756db69e19cfabbac87e1ffd789a523ba07
Toby Vincent <tobyv@tobyvin.dev>
2024-02-08 13:26:08 -0600

    feat(cliphist): move from clipman to cliphist

---
+++ cliphist/install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+pkgname=cliphist
+
+if ! command -v "$pkgname" >/dev/null; then
+	printf "%s: $pkgname not found, skipping...\n" "$0"
+	exit 0
+fi
+
+printf "%s: Installing services\n" "$0"
+
+systemctl --user enable --now --no-block $pkgname.service
