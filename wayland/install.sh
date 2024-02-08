aada61024e18a226ad090977af6f56976a608be1
Toby Vincent <tobyv@tobyvin.dev>
2024-02-08 13:45:19 -0600

    fix(wayland): enable services without starting

--- wayland/install.sh
+++ wayland/install.sh
@@ -4,7 +4,7 @@ printf "%s: Installing services\n" "$0"
 
 for pkgname in physlock wl-mpris-idle-inhibit; do
 	if command -v "$pkgname" >/dev/null; then
-		systemctl --user enable --now --no-block $pkgname.service
+		systemctl --user enable --no-block $pkgname.service
 	else
 		printf "%s: $pkgname not found, skipping...\n" "$0"
 	fi
