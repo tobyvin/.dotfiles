92c90eaac0c6c20ae35bb6657306d0ec320a7db6
Toby Vincent <tobyv@tobyvin.dev>
2024-03-01 13:37:46 -0600

    feat(nvim): add keymap for md checkbox toggle

--- nvim/.config/nvim/after/ftplugin/markdown.lua
+++ nvim/.config/nvim/after/ftplugin/markdown.lua
@@ -1,4 +1,38 @@
 vim.opt_local.textwidth = 80
 vim.g.markdown_recommended_style = 0
-vim.bo.softtabstop = 2
 vim.bo.shiftwidth = 2
+
+local function toggle_checkbox()
+	local node = vim.treesitter.get_node()
+
+	if node and not node:type():match("task_list_marker_(u?n?checked)") then
+		while node ~= nil and node:type() ~= "list_item" do
+			node = node:parent()
+		end
+
+		if node == nil or node:type() ~= "list_item" then
+			return false
+		end
+
+		node = vim.iter(node:iter_children()):find(function(child)
+			return child:type():match("task_list_marker_u?n?checked") ~= nil
+		end)
+	end
+
+	local content
+	if node == nil then
+		return
+	elseif node:type() == "task_list_marker_checked" then
+		content = { "[ ]" }
+	elseif node:type() == "task_list_marker_unchecked" then
+		content = { "[x]" }
+	end
+
+	local sr, sc, er, ec = node:range()
+	vim.api.nvim_buf_set_text(0, sr, sc, er, ec, content)
+end
+
+vim.keymap.set({ "n" }, "<leader><leader>", toggle_checkbox, {
+	buffer = 0,
+	desc = "toggle checkbox",
+})
