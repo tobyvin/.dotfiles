fc38b390df0dd36601ffdc1ea59ca3e00003270f
Toby Vincent <tobyv@tobyvin.dev>
2024-04-23 14:54:05 -0500

    chore(nvim,deps): update plugins

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -26,39 +26,19 @@ local M = {
 				end
 			end
 
-			local next_hunk = function()
-				if vim.wo.diff then
-					return "]c"
-				end
-				vim.schedule(function()
-					require("gitsigns").next_hunk({ preview = true })
-				end)
-				return "<Ignore>"
-			end
-
-			local prev_hunk = function()
-				if vim.wo.diff then
-					return "[c"
-				end
-				vim.schedule(function()
-					require("gitsigns").prev_hunk({ preview = true })
-				end)
-				return "<Ignore>"
-			end
-
-			vim.keymap.set("n", "]c", next_hunk, { expr = true, desc = "next hunk", buffer = bufnr })
-			vim.keymap.set("n", "[c", prev_hunk, { expr = true, desc = "previous hunk", buffer = bufnr })
-
 			vim.keymap.set("n", "<leader>gb", require("gitsigns").blame_line, { desc = "show blame", buffer = bufnr })
+
+			vim.keymap.set("n", "]c", function()
+				return require("gitsigns").nav_hunk("next", { preview = true })
+			end, { expr = true, desc = "next hunk", buffer = bufnr })
+
+			vim.keymap.set("n", "[c", function()
+				return require("gitsigns").nav_hunk("prev", { preview = true })
+			end, { expr = true, desc = "previous hunk", buffer = bufnr })
 
 			vim.keymap.set("n", "<leader>gB", function()
 				require("gitsigns").blame_line({ full = true })
 			end, { desc = "show blame", buffer = bufnr })
-
-			vim.keymap.set("n", "<leader>g<C-b>", require("gitsigns").toggle_current_line_blame, {
-				desc = "toggle blame",
-				buffer = bufnr,
-			})
 
 			vim.keymap.set({ "n", "v" }, "<leader>gr", with_range(require("gitsigns").reset_hunk), {
 				desc = "reset hunk",
@@ -67,11 +47,6 @@ local M = {
 
 			vim.keymap.set({ "n", "v" }, "<leader>gs", with_range(require("gitsigns").stage_hunk), {
 				desc = "stage hunk",
-				buffer = bufnr,
-			})
-
-			vim.keymap.set({ "n", "v" }, "<leader>gp", with_range(require("gitsigns").preview_hunk), {
-				desc = "preview hunk",
 				buffer = bufnr,
 			})
 
@@ -94,28 +69,8 @@ local M = {
 				desc = "undo stage buffer",
 				buffer = bufnr,
 			})
-
-			vim.api.nvim_exec_autocmds("User", { pattern = "GitAttach", data = { buf = bufnr } })
 		end,
 	},
 }
-
-function M:config(opts)
-	local popup = require("gitsigns.popup")
-	local popup_create = popup.create
-
-	---@diagnostic disable-next-line: duplicate-set-field
-	function popup.create(...)
-		local winid, bufnr = popup_create(...)
-
-		vim.keymap.set("n", "q", function()
-			pcall(vim.api.nvim_win_close, winid, true)
-		end, { buffer = bufnr })
-
-		return winid, bufnr
-	end
-
-	require("gitsigns").setup(opts)
-end
 
 return M
