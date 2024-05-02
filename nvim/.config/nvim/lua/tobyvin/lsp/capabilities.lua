def6e531db9db473c777610d084d1a5496894337
Toby Vincent <tobyv@tobyvin.dev>
2024-05-02 17:11:27 -0500

    fix(nvim): update for changes on master

--- nvim/.config/nvim/lua/tobyvin/lsp/capabilities.lua
+++ nvim/.config/nvim/lua/tobyvin/lsp/capabilities.lua
@@ -40,32 +40,6 @@ local M = {
 	[ms.textDocument_inlayHint] = function(bufnr)
 		vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
 	end,
-
-	-- in preparation for https://github.com/neovim/neovim/pull/28500
-	[ms.textDocument_signatureHelp] = function(bufnr)
-		vim.keymap.set("i", "<c-s>", vim.lsp.buf.signature_help, {
-			buffer = bufnr,
-			desc = "vim.lsp.buf.signature_help()",
-		})
-	end,
-	[ms.textDocument_references] = function(bufnr)
-		vim.keymap.set("n", "gr", vim.lsp.buf.references, {
-			buffer = bufnr,
-			desc = "vim.lsp.buf.references()",
-		})
-	end,
-	[ms.textDocument_rename] = function(bufnr)
-		vim.keymap.set("n", "crn", vim.lsp.buf.rename, {
-			buffer = bufnr,
-			desc = "vim.lsp.buf.rename()",
-		})
-	end,
-	[ms.textDocument_codeAction] = function(bufnr)
-		vim.keymap.set({ "n", "v" }, "crr", vim.lsp.buf.code_action, {
-			buffer = bufnr,
-			desc = "vim.lsp.buf.code_action()",
-		})
-	end,
 }
 
 return M
