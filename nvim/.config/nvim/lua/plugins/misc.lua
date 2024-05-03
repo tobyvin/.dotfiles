f3b82dc0839b15e83a365be7f3ab5b9659f5d159
Toby Vincent <tobyv@tobyvin.dev>
2024-05-03 11:18:50 -0500

    perf(nvim): remove usused plugins

--- nvim/.config/nvim/lua/plugins/misc.lua
+++ nvim/.config/nvim/lua/plugins/misc.lua
@@ -1,23 +1,6 @@
 ---@type LazySpec[]
 local M = {
 	{
-		"lukas-reineke/indent-blankline.nvim",
-		version = "*",
-		event = "BufReadPre",
-		main = "ibl",
-		opts = {
-			scope = {
-				show_start = false,
-				show_end = false,
-			},
-		},
-	},
-	{
-		"lukas-reineke/virt-column.nvim",
-		event = "BufAdd",
-		opts = {},
-	},
-	{
 		"eandrju/cellular-automaton.nvim",
 		cmd = "CellularAutomaton",
 	},
