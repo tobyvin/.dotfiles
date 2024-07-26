d8e8eb478150d334a24509fe9e1743f1cb7c0e1b
Toby Vincent <tobyv@tobyvin.dev>
2024-07-26 16:12:52 -0500

    fix(build): fix migrate script (merged pkg into src)

--- migrate.sh
+++ migrate.sh
@@ -5,19 +5,17 @@ if [ -d "$XDG_DATA_HOME/nvim/session" ]; then
 	mv -Tnv "$XDG_DATA_HOME/nvim/session" "$XDG_STATE_HOME/nvim/session"
 fi
 
-for dir in src pkg; do
-	if [ -d "$HOME"/$dir ]; then
-		printf '%s: Moving ~/%s directory into ~/.local\n' "$0" $dir
-		mv -Tnv "$HOME/src" "$HOME/.local/src"
-	fi
+if [ -d "$HOME"/src ]; then
+	printf '%s: Moving ~/src directory into ~/.local/src\n' "$0"
+	mv -Tnv "$HOME/src" "$HOME/.local/src"
 
 	for state_dir in undo session; do
-		for f in "$XDG_STATE_HOME/nvim/$state_dir/%home%tobyv%$dir%"*; do
+		printf '%s: Migrating nvim %s files to match new location\n' "$0" "$state_dir"
+		for f in "$XDG_STATE_HOME/nvim/$state_dir/%home%tobyv%src%"*; do
 			if [ -e "$f" ]; then
-				printf '%s: Migrating nvim %s files to match new location\n' "$0" "$state_dir"
 				sed -i 's|~/src/|~/.local/src|g' "$f"
-				printf '%s\n' "$f" | sed "s|/%home%tobyv%$dir%|/%home%tobyv%.local%$dir%|" | xargs mv -Tnv "$f"
+				printf '%s\n' "$f" | sed "s|/%home%tobyv%src%|/%home%tobyv%.local%src%|" | xargs mv -Tnv "$f"
 			fi
 		done
 	done
-done
+fi
