084603213bd04ee4cd1d68606bf3632c88d9b1fd
Toby Vincent <tobyv@tobyvin.dev>
2024-07-27 19:00:36 -0500

    fix: add another dotnet xdg_base_dir compat variable

--- sh/.config/profile.d/20-dotnet.sh
+++ sh/.config/profile.d/20-dotnet.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 export OMNISHARPHOME="$XDG_CONFIG_HOME/omnisharp"
+export NUGET_PACKAGES="$XDG_CACHE_HOME/NuGetPackages"
 export DOTNET_CLI_HOME="$XDG_DATA_HOME/dotnet"
 
 prepend_path "$DOTNET_CLI_HOME/tools"
