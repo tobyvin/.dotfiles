b3c586f1b177d5a898aa363188fa964c0ffd4e58
Toby Vincent <tobyv@tobyvin.dev>
2024-08-07 17:38:57 -0500

    build: simplify install script by use stow -R

--- install.sh
+++ install.sh
@@ -10,25 +10,13 @@ if [ -r .installed ]; then
 	export DOTFILES_INSTALLED
 fi
 
-printf "%s: Removing bad links\n" "$0"
-{
-	git log --name-only --no-renames --diff-filter=D --format=format: "$DOTFILES_INSTALLED" HEAD
-	git diff --name-only --no-renames --diff-filter=D HEAD
-	cat .untracked 2>/dev/null
-} | sort -u | while read -r f; do
-	if [ -L "$TARGET/${f#*/}" ] && [ ! -e "$TARGET/${f#*/}" ]; then
-		rm -v "$TARGET/${f#*/}"
-	fi
-done
-
-git ls-files --exclude-standard --others >.untracked
-if [ ! -s .untracked ]; then
-	rm -f .untracked
-fi
-
 printf "%s: Stowing packages\n" "$0"
 # shellcheck disable=SC2086
-stow "$@" ${1:-*}/
+stow -R "$@" ${1:-*}/ 2>&1 | tac | awk '
+	/\(reverts previous action\)$/ { reverted[$2] = 1; next }
+	/^LINK:/ { print $0 }
+	/^UNLINK:/ { if (!reverted[$2]) { print $0 } }
+' | tac
 
 printf "%s: Installing packages\n" "$0"
 for f in ${1:-*}/install.sh; do
