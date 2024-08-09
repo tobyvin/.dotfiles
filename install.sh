290849b1407f0bbb094d4620ee14bfdb5c3f3cb9
Toby Vincent <tobyv@tobyvin.dev>
2024-08-09 15:11:31 -0500

    fix: fix bug in install script's stow output filter

--- install.sh
+++ install.sh
@@ -12,11 +12,17 @@ fi
 
 printf "%s: Stowing packages\n" "$0"
 # shellcheck disable=SC2086
-stow -R "$@" ${1:-*}/ 2>&1 | tac | awk '
-	/\(reverts previous action\)$/ { reverted[$2] = 1; next }
-	/^LINK:/ { print $0 }
-	/^UNLINK:/ { if (!reverted[$2]) { print $0 } }
-' | tac
+stow -R "$@" ${1:-*}/ 2>&1 | awk '
+	/^UNLINK:/ { stdout[$0] = 1; next }
+	/\(reverts previous action\)$/ { delete stdout["UNLINK: " $2]; next }
+	/^LINK:/ { stdout[$0] = 1; next }
+	{ stderr[NR] = $0 }
+	END {
+		for (out in stdout) { print out };
+		for (err in stderr) { print stderr[err] > "/dev/stderr" }
+		if ( length(stderr) > 0 ) { exit 1 }
+	}
+'
 
 printf "%s: Installing packages\n" "$0"
 for f in ${1:-*}/install.sh; do
