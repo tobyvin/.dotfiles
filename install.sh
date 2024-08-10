216479bfc9f1c22b5de2a591a1a86668429e2dea
Toby Vincent <tobyv@tobyvin.dev>
2024-08-09 19:03:35 -0500

    fix: fix line ordering in stow output

--- install.sh
+++ install.sh
@@ -13,12 +13,12 @@ fi
 printf "%s: Stowing packages\n" "$0"
 # shellcheck disable=SC2086
 stow -R "$@" ${1:-*}/ 2>&1 | awk '
-	/^UNLINK:/ { stdout[$0] = 1; next }
-	/\(reverts previous action\)$/ { delete stdout["UNLINK: " $2]; next }
-	/^LINK:/ { stdout[$0] = 1; next }
+	/^UNLINK:/ { stdout[NR] = $0; unlinked[$2] = NR; next }
+	/\(reverts previous action\)$/ { delete stdout[unlinked[$2]]; next }
+	/^LINK:/ { stdout[NR] = $0; next }
 	{ stderr[NR] = $0 }
 	END {
-		for (out in stdout) { print out };
+		for (out in stdout) { print stdout[out] };
 		for (err in stderr) { print stderr[err] > "/dev/stderr" }
 		if ( length(stderr) > 0 ) { exit 1 }
 	}
