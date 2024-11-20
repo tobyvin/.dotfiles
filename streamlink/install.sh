dac99b5187a0349b4c106a0df56ffcc8e0e606cf
Toby Vincent <tobyv@tobyvin.dev>
2024-11-20 15:52:22 -0600

    feat(streamlink): add ttvlol twitch.py patch

---
+++ streamlink/install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+pkgname=streamlink
+
+if ! command -v "$pkgname" >/dev/null; then
+	printf '%s: %s not found, skipping...\n' "$0" "$pkgname"
+	exit 0
+fi
+
+if ! find "$XDG_DATA_HOME"/streamlink/plugins/twitch.py -mtime +7 -print | grep -q .; then
+	printf "%s: Installing plugins\n" "$0"
+
+	curl -Lo "$XDG_DATA_HOME"/streamlink/plugins/twitch.py \
+		'https://github.com/2bc4/streamlink-ttvlol/releases/latest/download/twitch.py'
+fi
