482332912eb182cffa8f888f2091d3c847c6566f
Toby Vincent <tobyv@tobyvin.dev>
2024-11-21 13:52:51 -0600

    fix(nvim): force osc52 over ssh due to alacritty...
    not supporting XTGETTCAP

--- nvim/.config/nvim/lua/tobyvin/options.lua
+++ nvim/.config/nvim/lua/tobyvin/options.lua
@@ -63,3 +63,36 @@ vim.api.nvim_create_autocmd("FileType", {
 		vim.opt_local.formatoptions:remove("o")
 	end,
 })
+
+local terms = {
+	"alacritty",
+}
+
+local tty = false
+for _, ui in ipairs(vim.api.nvim_list_uis()) do
+	if ui.chan == 1 and ui.stdout_tty then
+		tty = true
+		break
+	end
+end
+
+if
+	tty
+	and (vim.g.clipboard == nil or vim.o.clipboard == "")
+	and os.getenv("SSH_TTY")
+	and vim.list_contains(terms, vim.env.TERM)
+then
+	local osc52 = require("vim.ui.clipboard.osc52")
+
+	vim.g.clipboard = {
+		name = "OSC 52",
+		copy = {
+			["+"] = osc52.copy("+"),
+			["*"] = osc52.copy("*"),
+		},
+		paste = {
+			["+"] = osc52.paste("+"),
+			["*"] = osc52.paste("*"),
+		},
+	}
+end
