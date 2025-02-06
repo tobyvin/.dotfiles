ba963dc3a046e77bcee1fd4bd916085bb161c655
Toby Vincent <tobyv@tobyvin.dev>
2025-02-06 16:10:56 -0600

    build: fix error detection bug in awk script

--- install.sh
+++ install.sh
@@ -16,14 +16,16 @@ export DOTFILES_INSTALLED
 printf "%s: Stowing packages\n" "$0"
 # shellcheck disable=SC2086
 stow -R "$@" ${1:-*}/ 2>&1 | awk '
+	BEGIN { code = 0 }
 	/^UNLINK:/ { stdout[NR] = $0; unlinked[$2] = NR; next }
 	/\(reverts previous action\)$/ { delete stdout[unlinked[$2]]; next }
 	/^LINK:/ { stdout[NR] = $0; next }
+	/^All operations aborted./ { stderr[NR] = $0; err = 1; next }
 	{ stderr[NR] = $0 }
 	END {
-		for (out in stdout) { print stdout[out] };
-		for (err in stderr) { print stderr[err] > "/dev/stderr" }
-		if ( length(stderr) > 0 ) { exit 1 }
+		for (i in stdout) { print stdout[i] };
+		for (i in stderr) { print stderr[i] > "/dev/stderr" }
+		if ( length(stderr) > 0 ) { exit err }
 	}
 '
 
