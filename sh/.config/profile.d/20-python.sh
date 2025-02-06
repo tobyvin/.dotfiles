9d71b1669823842beb9059ca5c1fd898b9d751f9
Toby Vincent <tobyv@tobyvin.dev>
2025-02-06 16:12:30 -0600

    fix: add more xdg base dir compliance stuff

--- sh/.config/profile.d/20-python.sh
+++ sh/.config/profile.d/20-python.sh
@@ -3,3 +3,4 @@
 export PYTHONUSERBASE="$XDG_DATA_HOME/python"
 export PYTHONPYCACHEPREFIX="$XDG_CACHE_HOME/python"
 export PYTHON_HISTORY="$XDG_STATE_HOME/python/history"
+export CONDARC="$XDG_CONFIG_HOME/conda/condarc"
---
+++ sh/.config/profile.d/50-java.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+export _JAVA_OPTIONS=-Djava.util.prefs.userRoot="$XDG_CONFIG_HOME"/java
