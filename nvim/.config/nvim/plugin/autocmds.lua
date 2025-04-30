06cb502f77b7ded570aba40afa603091ef4b9cc3
Toby Vincent <tobyv@tobyvin.dev>
2025-04-30 11:45:23 -0500

    feat(nvim): trust .nvim.lua on write

--- nvim/.config/nvim/plugin/autocmds.lua
+++ nvim/.config/nvim/plugin/autocmds.lua
@@ -1,5 +1,6 @@
+local augroup = vim.api.nvim_create_augroup("user", { clear = true })
 vim.api.nvim_create_autocmd("TextYankPost", {
-	group = vim.api.nvim_create_augroup("user", { clear = true }),
+	group = augroup,
 	pattern = "*",
 	callback = function()
 		vim.highlight.on_yank()
@@ -9,8 +10,21 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 
 -- Override ftplugins that override formatoptions
 vim.api.nvim_create_autocmd("FileType", {
-	group = vim.api.nvim_create_augroup("user.options", { clear = true }),
+	group = augroup,
 	callback = function()
 		vim.opt_local.formatoptions:remove("o")
 	end,
+	desc = "Override ftplugins that override formatoptions",
+})
+
+vim.api.nvim_create_autocmd("BufWritePost", {
+	group = augroup,
+	pattern = ".nvim.lua",
+	callback = function()
+		vim.secure.trust({
+			action = "allow",
+			bufnr = vim.api.nvim_get_current_buf(),
+		})
+	end,
+	desc = "Trust exrc files after write",
 })
