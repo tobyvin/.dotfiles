f01d28a665e353b0ba1985bc95d7ec27f9c1459c
Toby Vincent <tobyv@tobyvin.dev>
2025-05-20 15:03:09 -0500

    feat(nvim): add mpv types to luals

--- nvim/.config/nvim/lua/plugins/lazydev.lua
+++ nvim/.config/nvim/lua/plugins/lazydev.lua
@@ -5,6 +5,7 @@ local M = {
 	"LuaCATS/tex-lualatex",
 	"LuaCATS/tex-luametatex",
 	"LuaCATS/tex-lualibs",
+	"disco0/mpv-types-lua",
 	{
 		"https://gitlab.com/carsakiller/cc-tweaked-documentation",
 		name = "cc-tweaked",
@@ -16,11 +17,12 @@ local M = {
 		opts = {
 			library = {
 				"lazy.nvim",
-				{ path = "luvit-meta/library", words = { "vim%.uv" } },
+				{ path = "luvit-meta", words = { "vim%.uv" } },
 				{ path = "tex-luatex", words = { "tex" } },
 				{ path = "tex-lualatex", words = { "tex" } },
 				{ path = "tex-luametatex", words = { "tex" } },
 				{ path = "tex-lualibs", words = { "tex" } },
+				{ path = "mpv-types-lua/types", mods = { "mp", "mp.msg", "mp.options", "mp.assdraw", "mp.utils" } },
 				{ path = "cc-tweaked", words = { "cc" } },
 			},
 			integrations = {
