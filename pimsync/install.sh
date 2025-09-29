88041ebaea6986a951f7aa65d84eb619b832d2e0
Toby Vincent <tobyv@tobyvin.dev>
2025-09-29 15:40:26 -0500

    feat(pimsync): move from vdirsyncer to pimsync

--- pimsync/install.sh
+++ pimsync/install.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-pkgname=vdirsyncer
+pkgname=pimsync
 
 if ! command -v "$pkgname" >/dev/null; then
 	printf '%s: %s not found, skipping...\n' "$0" "$pkgname"
 	exit 0
 fi
 
+mkdir -pv "${XDG_DATA_HOME}/calendars" "${XDG_DATA_HOME}/contacts"
+
 printf "%s: Installing services\n" "$0"
 
-systemctl --user enable --no-block vdirsyncer.timer
+systemctl --user enable --now --no-block pimsync.service
