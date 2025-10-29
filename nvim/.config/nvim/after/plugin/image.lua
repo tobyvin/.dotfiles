44369677d5ff85052255cf54ded7aabd64644145
Toby Vincent <tobyv@tobyvin.dev>
2025-10-29 14:37:31 -0500

    fix(nvim): only enable image.nvim if magick is installed

--- nvim/.config/nvim/after/plugin/image.lua
+++ nvim/.config/nvim/after/plugin/image.lua
@@ -1,6 +1,6 @@
 -- Don't load if running headless due to error getting term size on the follow line,
 -- https://github.com/3rd/image.nvim/blob/446a8a5cc7a3eae3185ee0c697732c32a5547a0b/lua/image/utils/term.lua#L34
-if #vim.api.nvim_list_uis() == 0 then
+if vim.fn.executable("magick") == 0 or #vim.api.nvim_list_uis() == 0 then
 	return
 end
 require("image").setup({
