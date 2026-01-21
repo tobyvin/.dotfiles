d04125bfee1a6609943e698844651ab0754e2818
Toby Vincent <tobyv@tobyvin.dev>
2026-01-21 13:00:14 -0600

    fix(nvim): revert to simple package hook for updates

--- nvim/.config/nvim/plugin/pack.lua
+++ nvim/.config/nvim/plugin/pack.lua
@@ -1,61 +1,106 @@
 local latest = vim.version.range("*")
+local gh = function(stub)
+	return "https://github.com/" .. stub
+end
+local gl = function(stub)
+	return "https://gitlab.com/" .. stub
+end
+
+local hooks = function(args)
+	local pkg = args.data
+	local name, kind = pkg.spec.name, pkg.kind
+	local success, res
+
+	-- HACK: fix for vim.pack not printing newlines when --headless
+	if #vim.api.nvim_list_uis() == 0 then
+		vim.print("\n")
+	end
+
+	if name == "telescope-fzf-native.nvim" and (kind == "install" or kind == "update") then
+		success, res = pcall(vim.system, { "make" }, { cwd = pkg.path })
+		if success and #vim.api.nvim_list_uis() == 0 then
+			res:wait(300000)
+		end
+	end
+
+	if name == "nvim-texlabconfig" and (kind == "install" or kind == "update") then
+		success, res = pcall(
+			vim.system,
+			{ "go", "build", "-o", vim.fs.joinpath(vim.env.HOME, ".local/bin/") },
+			{ cwd = pkg.path }
+		)
+		if success and #vim.api.nvim_list_uis() == 0 then
+			res:wait(300000)
+		end
+	end
+
+	if name == "mason.nvim" and kind == "update" then
+		if not pkg.active then
+			vim.cmd.packadd("mason.nvim")
+		end
+		success, res = pcall(vim.cmd["MasonUpdate"])
+	end
+
+	if name == "nvim-treesitter" and kind == "update" then
+		if not pkg.active then
+			vim.cmd.packadd("nvim-treesitter")
+		end
+		success, res = pcall(require("nvim-treesitter").update)
+		if success and #vim.api.nvim_list_uis() == 0 then
+			res:wait(300000)
+		end
+	end
+
+	if success == false then
+		vim.notify(
+			("Failed to build - %s: %s"):format(pkg.spec.name, res),
+			vim.log.levels.ERROR,
+			{ title = "vim.pack" }
+		)
+		return
+	end
+end
+
+vim.api.nvim_create_autocmd("PackChanged", { callback = hooks })
 
 vim.pack.add({
-	"https://github.com/ellisonleao/gruvbox.nvim",
-	"https://github.com/nvim-lua/plenary.nvim",
-	{
-		src = "https://github.com/nvim-treesitter/nvim-treesitter",
-		version = "main",
-		data = {
-			build = function()
-				local update = require("nvim-treesitter").update()
-				if #vim.api.nvim_list_uis() == 0 then
-					update:wait(300000)
-				end
-			end,
-		},
-	},
-	{ src = "https://github.com/nvim-treesitter/nvim-treesitter-textobjects", version = "main" },
-	{ src = "https://github.com/stevearc/oil.nvim", version = latest },
-	{ src = "https://github.com/williamboman/mason.nvim", version = latest, data = {
-		build = "MasonUpdate",
-	} },
-	{ src = "https://github.com/stevearc/conform.nvim", version = latest },
-	"https://github.com/codethread/qmk.nvim",
-	"https://github.com/mfussenegger/nvim-lint",
-	{ src = "https://github.com/lewis6991/gitsigns.nvim", version = latest },
-	{ src = "https://github.com/j-hui/fidget.nvim", version = latest },
-	"https://github.com/nvim-telescope/telescope.nvim",
-	"https://github.com/nvim-telescope/telescope-live-grep-args.nvim",
-	"https://github.com/debugloop/telescope-undo.nvim",
-	{ src = "https://github.com/nvim-telescope/telescope-fzf-native.nvim", data = {
-		build = { "make" },
-	} },
-	{ src = "https://github.com/3rd/image.nvim", version = latest },
-	"https://github.com/kiyoon/magick.nvim",
-	{ src = "https://github.com/folke/lazydev.nvim", version = latest },
-	"https://github.com/Bilal2453/luvit-meta",
-	"https://github.com/LuaCATS/tex-luatex",
-	"https://github.com/LuaCATS/tex-lualatex",
-	"https://github.com/LuaCATS/tex-luametatex",
-	"https://github.com/LuaCATS/tex-lualibs",
-	"https://github.com/disco0/mpv-types-lua",
-	"https://gitlab.com/carsakiller/cc-tweaked-documentation.git",
-	{
-		src = "https://github.com/f3fora/nvim-texlabconfig",
-		data = { build = { "go", "build", "-o", vim.fs.joinpath(vim.env.HOME, ".local/bin/") } },
-	},
-	"https://github.com/vxpm/ferris.nvim",
-	"https://github.com/mfussenegger/nvim-jdtls",
-	"https://github.com/microsoft/python-type-stubs",
-	"https://github.com/b0o/SchemaStore.nvim",
-	"https://github.com/tridactyl/vim-tridactyl",
-	"https://github.com/hjson/vim-hjson",
-	"https://github.com/nfnty/vim-nftables",
-	"https://github.com/wilriker/gcode.vim",
-	"https://github.com/andweeb/presence.nvim",
-	"https://github.com/eandrju/cellular-automaton.nvim",
-	"https://github.com/rktjmp/playtime.nvim",
+	gh("ellisonleao/gruvbox.nvim"),
+	gh("nvim-lua/plenary.nvim"),
+	{ src = gh("nvim-treesitter/nvim-treesitter"), version = "main" },
+	{ src = gh("nvim-treesitter/nvim-treesitter-textobjects"), version = "main" },
+	{ src = gh("stevearc/oil.nvim"), version = latest },
+	{ src = gh("williamboman/mason.nvim"), version = latest },
+	{ src = gh("stevearc/conform.nvim"), version = latest },
+	gh("codethread/qmk.nvim"),
+	gh("mfussenegger/nvim-lint"),
+	{ src = gh("lewis6991/gitsigns.nvim"), version = latest },
+	{ src = gh("j-hui/fidget.nvim"), version = latest },
+	gh("nvim-telescope/telescope.nvim"),
+	gh("nvim-telescope/telescope-live-grep-args.nvim"),
+	gh("debugloop/telescope-undo.nvim"),
+	gh("nvim-telescope/telescope-fzf-native.nvim"),
+	{ src = gh("3rd/image.nvim"), version = latest },
+	gh("kiyoon/magick.nvim"),
+	{ src = gh("folke/lazydev.nvim"), version = latest },
+	gh("Bilal2453/luvit-meta"),
+	gh("LuaCATS/tex-luatex"),
+	gh("LuaCATS/tex-lualatex"),
+	gh("LuaCATS/tex-luametatex"),
+	gh("LuaCATS/tex-lualibs"),
+	gh("disco0/mpv-types-lua"),
+	gl("carsakiller/cc-tweaked-documentation.git"),
+	gh("f3fora/nvim-texlabconfig"),
+	gh("vxpm/ferris.nvim"),
+	gh("mfussenegger/nvim-jdtls"),
+	gh("microsoft/python-type-stubs"),
+	gh("b0o/SchemaStore.nvim"),
+	gh("tridactyl/vim-tridactyl"),
+	gh("hjson/vim-hjson"),
+	gh("nfnty/vim-nftables"),
+	gh("wilriker/gcode.vim"),
+	gh("andweeb/presence.nvim"),
+	gh("eandrju/cellular-automaton.nvim"),
+	gh("rktjmp/playtime.nvim"),
 }, {
 	load = true,
 	confirm = #vim.api.nvim_list_uis() ~= 0,
--- nvim/.config/nvim/plugin/pack/build.lua
+++ nvim/.config/nvim/plugin/pack/build.lua
@@ -1,37 +1,38 @@
-vim.api.nvim_create_autocmd("PackChanged", {
-	group = vim.api.nvim_create_augroup("build_system", { clear = true }),
-	pattern = "*",
-	callback = function(args)
-		local pkg = args.data
-		local build = (pkg.spec.data or {}).build
-		local success, res
-		-- HACK: fix for vim.pack not printing newlines when --headless
-		if #vim.api.nvim_list_uis() == 0 then
-			vim.print("\n")
-		end
-		if type(build) == "table" and (pkg.kind == "install" or pkg.kind == "update") then
-			success, res = pcall(vim.system, build, { cwd = pkg.path })
-			if success and #vim.api.nvim_list_uis() == 0 then
-				res:wait(300000)
-			end
-		elseif pkg.kind == "update" then
-			if not pkg.active then
-				vim.cmd.packadd(pkg.spec.name)
-			end
-			if type(build) == "function" then
-				success, res = pcall(build, pkg)
-			elseif type(build) == "string" then
-				success, res = pcall(vim.cmd[build])
-			end
-		end
-
-		if success == false then
-			vim.notify(
-				("Failed to build - %s: %s"):format(pkg.spec.name, res),
-				vim.log.levels.ERROR,
-				{ title = "vim.pack" }
-			)
-			return
-		end
-	end,
-})
+--- Does not work. Package spec's .data is not passed to this for some reason
+-- vim.api.nvim_create_autocmd("PackChanged", {
+-- 	group = vim.api.nvim_create_augroup("build_system", { clear = true }),
+-- 	pattern = "*",
+-- 	callback = function(args)
+-- 		local pkg = args.data
+-- 		local build = (pkg.spec.data or {}).build
+-- 		local success, res
+-- 		-- HACK: fix for vim.pack not printing newlines when --headless
+-- 		if #vim.api.nvim_list_uis() == 0 then
+-- 			vim.print("\n")
+-- 		end
+-- 		if type(build) == "table" and (pkg.kind == "install" or pkg.kind == "update") then
+-- 			success, res = pcall(vim.system, build, { cwd = pkg.path })
+-- 			if success and #vim.api.nvim_list_uis() == 0 then
+-- 				res:wait(300000)
+-- 			end
+-- 		elseif pkg.kind == "update" then
+-- 			if not pkg.active then
+-- 				vim.cmd.packadd(pkg.spec.name)
+-- 			end
+-- 			if type(build) == "function" then
+-- 				success, res = pcall(build, pkg)
+-- 			elseif type(build) == "string" then
+-- 				success, res = pcall(vim.cmd[build])
+-- 			end
+-- 		end
+--
+-- 		if success == false then
+-- 			vim.notify(
+-- 				("Failed to build - %s: %s"):format(pkg.spec.name, res),
+-- 				vim.log.levels.ERROR,
+-- 				{ title = "vim.pack" }
+-- 			)
+-- 			return
+-- 		end
+-- 	end,
+-- })
