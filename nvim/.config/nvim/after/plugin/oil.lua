e31b26edf2a015636cfd59f56f800e6a42b922ee
Toby Vincent <tobyv@tobyvin.dev>
2026-02-10 14:24:36 -0600

    fix(nvim): clean up oil.nvim hack

--- nvim/.config/nvim/after/plugin/oil.lua
+++ nvim/.config/nvim/after/plugin/oil.lua
@@ -72,70 +72,31 @@ oil.setup({
 	},
 })
 
----@param url string
----@param callback fun(url: string)
----@diagnostic disable-next-line: duplicate-set-field
-require("oil.adapters.files").normalize_url = function(url, callback)
-	local fs = require("oil.fs")
-	local util = require("oil.util")
+-- HACK: stops symlinks from being resolved when opening buffers
+do
+	local normalize_url = require("oil.adapters.files").normalize_url
 
-	local scheme, path = util.parse_url(url)
-	assert(path)
-
-	if fs.is_windows then
-		if path == "/" then
-			return callback(url)
-		else
-			local is_root_drive = path:match("^/%u$")
-			if is_root_drive then
-				return callback(url .. "/")
-			end
+	---@param url string
+	---@param callback fun(url: string)
+	---@diagnostic disable-next-line: duplicate-set-field
+	require("oil.adapters.files").normalize_url = function(url, callback)
+		local fs_realpath = vim.uv.fs_realpath
+		---@diagnostic disable-next-line: duplicate-set-field
+		vim.uv.fs_realpath = function(os_path, realpath_cb)
+			return vim.uv.fs_lstat(
+				os_path,
+				vim.schedule_wrap(function(_, stat)
+					if stat and stat.type == "link" then
+						return realpath_cb(nil, os_path)
+					else
+						return vim.uv.fs_realpath(os_path, realpath_cb)
+					end
+				end)
+			)
 		end
+		normalize_url(url, callback)
+		vim.uv.fs_realpath = fs_realpath
 	end
-
-	local os_path = vim.fn.fnamemodify(fs.posix_to_os_path(path), ":p")
-
-	local realpath_cb = function(_, new_os_path)
-		local realpath
-		if fs.is_windows then
-			realpath = os_path
-		else
-			realpath = new_os_path or os_path
-		end
-
-		vim.uv.fs_stat(
-			realpath,
-			vim.schedule_wrap(function(_, stat)
-				local is_directory
-				if stat then
-					is_directory = stat.type == "directory"
-				elseif vim.endswith(realpath, "/") or (fs.is_windows and vim.endswith(realpath, "\\")) then
-					is_directory = true
-				else
-					local filetype = vim.filetype.match({ filename = vim.fs.basename(realpath) })
-					is_directory = filetype == nil
-				end
-
-				if is_directory then
-					local norm_path = util.addslash(fs.os_to_posix_path(realpath))
-					callback(scheme .. norm_path)
-				else
-					callback(realpath)
-				end
-			end)
-		)
-	end
-
-	vim.uv.fs_lstat(
-		os_path,
-		vim.schedule_wrap(function(_, stat)
-			if stat and stat.type == "link" then
-				realpath_cb(nil, os_path)
-			else
-				vim.uv.fs_realpath(os_path, realpath_cb)
-			end
-		end)
-	)
 end
 
 vim.keymap.set("n", "-", oil.open, { desc = "Open parent directory" })
