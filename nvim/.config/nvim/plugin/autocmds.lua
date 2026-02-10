0bd880125ef60f48a81d1e85ef0ba6c27b2889d6
Toby Vincent <tobyv@tobyvin.dev>
2026-02-10 14:25:05 -0600

    fix(nvim): fix depreciated function

--- nvim/.config/nvim/plugin/autocmds.lua
+++ nvim/.config/nvim/plugin/autocmds.lua
@@ -3,7 +3,7 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	group = augroup,
 	pattern = "*",
 	callback = function()
-		vim.highlight.on_yank()
+		vim.hl.on_yank()
 	end,
 	desc = "vim.highlight.on_yank()",
 })
