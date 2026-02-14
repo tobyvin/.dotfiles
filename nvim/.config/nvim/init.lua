ec76de8ad6ea8ea1b18b77caa0fd73465e4c32b1
Toby Vincent <tobyv@tobyvin.dev>
2026-02-14 12:53:23 -0600

    fix(nvim): load options and autocmds before plugins

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,3 +1,97 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = vim.g.mapleader
 vim.g.loaded_perl_provider = 0
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+vim.opt.background = "dark"
+vim.opt.breakindent = true
+vim.opt.colorcolumn = "+1"
+vim.opt.complete = "o,F,.,w,b,u,t"
+vim.opt.completeopt = { "menuone", "noselect", "noinsert", "popup", "preinsert" }
+vim.opt.conceallevel = 2
+vim.opt.cursorline = true
+vim.opt.dictionary = { "spell", "/usr/share/dict/words" }
+vim.opt.diffopt = { "internal", "filler", "closeoff", "hiddenoff" }
+vim.opt.equalalways = false
+vim.opt.expandtab = false
+vim.opt.exrc = true
+vim.opt.fillchars:append("fold: ")
+vim.opt.foldenable = false
+vim.opt.foldlevelstart = 99
+vim.opt.foldmethod = "expr"
+vim.opt.foldtext = ""
+vim.opt.formatoptions = "cqrnj"
+vim.opt.ignorecase = true
+vim.opt.inccommand = "split"
+vim.opt.jumpoptions:append("stack")
+vim.opt.linebreak = true
+vim.opt.list = true
+vim.opt.modeline = true
+vim.opt.mouse = "a"
+vim.opt.number = true
+vim.opt.pumblend = 15
+vim.opt.pumheight = 10
+vim.opt.pummaxwidth = 60
+vim.opt.pumwidth = 30
+vim.opt.relativenumber = true
+vim.opt.scrolloff = 10
+vim.opt.sessionoptions = { "buffers", "curdir", "folds", "help", "tabpages", "winsize", "winpos" }
+vim.opt.shiftround = true
+vim.opt.shiftwidth = 0
+vim.opt.shortmess:append("a")
+vim.opt.showbreak = string.rep(" ", 3)
+vim.opt.signcolumn = "yes:1"
+vim.opt.smartcase = true
+vim.opt.smartindent = true
+vim.opt.smoothscroll = true
+vim.opt.spelloptions = { "camel" }
+vim.opt.softtabstop = -1
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.swapfile = false
+vim.opt.switchbuf = { "useopen", "uselast" }
+vim.opt.termguicolors = true
+vim.opt.textwidth = 99
+vim.opt.timeoutlen = 500
+vim.opt.undofile = true
+vim.opt.undolevels = 500
+vim.opt.updatetime = 500
+vim.opt.wildignore:append({ "*.o", "*.rej", "*.so", "*~", "*.pyc", "*pycache*", "Cargo.lock" })
+vim.opt.wildmode = { "longest:full", "full", "noselect" }
+vim.opt.winborder = "single"
+vim.opt.wrap = false
+vim.opt.wrapscan = false
+
+vim.cmd.packadd("nvim.undotree")
+
+local augroup = vim.api.nvim_create_augroup("user", { clear = true })
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+	group = augroup,
+	pattern = "*",
+	callback = function()
+		vim.hl.on_yank()
+	end,
+	desc = "Highlight text on yank",
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+	group = augroup,
+	callback = function()
+		vim.opt_local.formatoptions:remove("o")
+	end,
+	desc = "Override ftplugins that override formatoptions",
+})
+
+vim.api.nvim_create_autocmd("BufWritePost", {
+	group = augroup,
+	pattern = ".nvim.lua",
+	callback = function()
+		vim.secure.trust({
+			action = "allow",
+			bufnr = vim.api.nvim_get_current_buf(),
+		})
+	end,
+	desc = "Trust exrc files after write",
+})
--- nvim/.config/nvim/plugin/builtin.lua
+++
@@ -1,5 +0,0 @@
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
-vim.cmd.packadd("nvim.undotree")
-vim.cmd.packadd("nvim.difftool")
