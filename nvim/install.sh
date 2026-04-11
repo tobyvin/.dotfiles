613e7eb16101a8f204350bb10bfc138da47564ec
Toby Vincent <tobyv@tobyvin.dev>
2026-04-11 17:33:07 -0500

    feat(nvim): improve install script

--- nvim/install.sh
+++ nvim/install.sh
@@ -7,10 +7,14 @@ if ! command -v "$pkgname" >/dev/null; then
 	exit 0
 fi
 
-printf "%s: Installing plugins" "$0"
+printf "%s: Installing plugins\n" "$0"
+nvim --headless -c ':lua vim.pack.update(nil, { force = true, target = "lockfile" })' -c qa
 
-printf "\n"
-nvim --headless -c ':lua vim.pack.update(nil, { force = true, target = "lockfile" }) vim.pack.clean(nil, { inactive = true })' -c qa
+printf "\n%s: Cleaning inactive plugins" "$0"
+nvim --headless -c ':lua vim.pack.clean(nil, { inactive = true })' -c qa
+
+printf "\n%s: Downloading spellfile" "$0"
+nvim --headless -c ':lua require("nvim.spellfile").get("english")' -c qa
 printf "\n"
 
 if [ -d "$XDG_DATA_HOME"/nvim/lazy ] || [ -d "$XDG_STATE_HOME"/nvim/lazy ]; then
